bf3b9660c439b8f890d9fd80456313cd3e90e1ff
piyussssh <mvps1862@gmail.com>
2023-04-06 16:11:22 +0530

    404 page created

--- pages/[contry]/[pageNo].js
+++ pages/[contry]/[pageNo].js
@@ -1,15 +1,23 @@
 import { useRouter } from "next/router";
 import data from "../content.json";
-const pageNo = () =>{
+const pageNo = () => {
   const router = useRouter();
-  const page = router.query.pageNo;
-  const contry = router.query.contry;
-  return(
+  const pageNo = router.query.pageNo;
+  const country = router.query.contry;
+  const countryData = data[country] || {};
+  const pageData = countryData[pageNo];
+  if (!countryData || !pageData) {
+    return (
+      <div>
+        <h1>404 - Page Not Found</h1>
+        <p>The requested page was not found.</p>
+      </div>
+    );
+  }
+  return (
     <>
-    <h1>
-      {data[contry]?.[page]?.head}
-    </h1>
+      <h1>{pageData.head}</h1>
     </>
-  )
+  );
 };
 export default pageNo;
