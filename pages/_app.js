65ca6580c18f1b46c85e665c1f53cc8fd7da66c1
AshishYadav <ashish.y@growthways.io>
2023-04-12 18:41:16 +0530

    code pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,12 @@ import "@/styles/globals.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "@/components/navbar";
 import Headcomp from "@/components/head";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return (
     <>
       <Headcomp/>
