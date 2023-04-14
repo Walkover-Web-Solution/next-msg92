bc499b92f37c17e625f6b2acfc2096047d8b0426
piyussssh <mvps1862@gmail.com>
2023-04-14 18:39:41 +0530

    contact us designed

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -4,57 +4,61 @@ import { InlineWidget } from "react-calendly";
 const contactus = () => {
     return (
         <>
-            <div className="container text-center px-4 col-xs-12 col-sm-10 col-md-10 mx-auto section py-3">
-                <h1 className="sub-heading pb-2">Contact Us</h1>
+            <div className="container text-center px-4 col-xs-12 col-sm-10 col-md-10 mx-auto  py-5">
+                <h1 className="sub-heading pb-5">Contact Us</h1>
                 <div className="d-flex justify-content-around">
-                    <div className="contact-card shadow col-4">
+                    <div className="contact-card d-flex flex-column align-items-center justify-content-between shadow col-4 gap-5 px-3 py-5">
                         <h2>Sales</h2>
-                        <div className="w-100 d-flex flex-column align-items-center">
+                        <div className="d-flex flex-column align-items-start gap-4">
+                        <div className="d-flex  align-items-center">
                             <span className="c-fs-1   rounded-circle social-circle justify-content-center align-items-center d-flex">
-                                <MdOutlineEmail className="p-auto c-fw-sb text-primary" />
+                                <MdOutlineEmail className="p-auto c-fw-sb  " />
                             </span>
-                            <a href="mailto:sales@msg91.com"  className="c-fs-2 px-3 py-3 mt-n2 rounded w-75 text-center bg-white">
+                            <a href="mailto:sales@msg91.com"  className="c-fs-2 ms-3 text-dark text-center bg-white">
                                 sales@msg91.com
                             </a>
                         </div>
-                        <div className="w-100 d-flex flex-column align-items-center mt-4 mt-sm-0 mt-lg-4">
+                        <div className="d-flex  align-items-center">
                             <span className="c-fs-1 c-fw-b  rounded-circle social-circle justify-content-center align-items-center d-flex">
-                                <MdAddCall className="p-auto text-primary" />
+                                <MdAddCall className="p-auto  " />
                             </span>
-                            <a href="tel:91987654321" className="c-fs-2 px-3 py-3 mt-n2 rounded w-75 text-center bg-white">
+                            <a href="tel:91987654321" className="c-fs-2 ms-3 text-dark text-center bg-white">
                                 +91-987654321
                             </a>
+                        </div>
                         </div>
                         <button className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#sales-modal">Schedule a meeting</button>
                     </div>
 
-                    <div className="contact-card shadow col-4">
+                    <div className="contact-card d-flex flex-column align-items-center justify-content-between shadow col-4 gap-5 px-3 py-5">
                         <h2>Support</h2>
-                        <div className="w-100 d-flex flex-column align-items-center">
+                        <div className="d-flex flex-column align-items-start gap-4">
+                        <div className="d-flex  align-items-center">
                             <span className="c-fs-1   rounded-circle social-circle justify-content-center align-items-center d-flex">
-                                <MdOutlineEmail className="p-auto c-fw-sb text-primary" />
+                                <MdOutlineEmail className="p-auto c-fw-sb  " />
                             </span>
-                            <a href="mailto:support@msg91.com" className="c-fs-2 px-3 py-3 mt-n2 rounded w-75 text-center bg-white">
+                            <a href="mailto:support@msg91.com" className="c-fs-2 ms-3 text-dark text-center bg-white">
                                 support@msg91.com
                             </a>
                         </div>
                         
-                        <div className="w-100 d-flex flex-column align-items-center mt-4 mt-sm-0 mt-lg-4">
+                        <div className="d-flex  align-items-center">
                             <span className="c-fs-1 c-fw-b  rounded-circle social-circle justify-content-center align-items-center d-flex">
-                                <MdWhatsapp className="p-auto text-primary" />
+                                <MdWhatsapp className="p-auto  " />
                             </span>
-                            <a href="https://wa.me/918818888733" className="c-fs-2 px-3 py-3 mt-n2 rounded w-75 text-center bg-white">
+                            <a href="https://wa.me/918818888733" className="c-fs-2 text-dark ms-3 text-center bg-white">
                                 +91 88188 88733                
                             </a>
                         </div>
 
-                        <div className="w-100 d-flex flex-column align-items-center mt-4 mt-sm-0 mt-lg-4">
+                        <div className="d-flex  align-items-center">
                             <span className="c-fs-1 c-fw-b  rounded-circle social-circle justify-content-center align-items-center d-flex">
-                                <MdAddCall className="p-auto text-primary" />
+                                <MdAddCall className="p-auto  " />
                             </span>
-                            <a href="tel:918818888733" className="c-fs-2 px-3 py-3 mt-n2 rounded w-75 text-center bg-white">
+                            <a href="tel:918818888733" className="c-fs-2  ms-3 text-dark text-center bg-white">
                                 +91 88188 88733
                             </a>
+                        </div>
                         </div>
                         
                         <button className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#support-modal">Schedule a meeting</button>
