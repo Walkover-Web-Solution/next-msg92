f966ced1a18bfe845a187416cf2feb206c4eed2f
piyussssh <mvps1862@gmail.com>
2023-04-18 11:08:22 +0530

    push

--- components/navbar.js
+++ components/navbar.js
@@ -40,7 +40,7 @@ const navbar = ({brawserPath}) => {
   return (
     <>
       <Notification />
-      <Mininav/>
+      {/* <Mininav path={path}/> */}
       <nav className="w-100 py-2 sticy align-items-center d-flex " id="navbar">
         <div className="container relative d-flex align-items-center justify-content-between c-fs-4 c-fw-m nav-menu-cont">
           <div className=" d-flex gap-5 w-nav-menu align-items-center justify-content-start nav-menu">            
