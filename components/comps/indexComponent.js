c21ff86fe8d56d6ee76422a0d7f9725c331428f8
piyussssh <mvps1862@gmail.com>
2023-04-21 11:16:20 +0530

    index res

--- components/comps/indexComponent.js
+++ components/comps/indexComponent.js
@@ -59,7 +59,7 @@ const indexComp = ({ params }) => {
           </div>
           <div className="product-content  p-4 d-flex flex-column justify-content-between gap-3">
             <p className="pro-text text-white">
-            Segment your customers according to their action or event
+              Segment your customers according to their action or event
             </p>
             <a
               href="/hello"
@@ -126,7 +126,7 @@ const indexComp = ({ params }) => {
 
       <div className="container my-5 my-80">
         <h2 className="sub-heading c-head mb-2">Communication Channels</h2>
-        <p className="c-fs-2 w-75">
+        <p className="c-fs-2 col-12 col-md-11 col-lg-9">
           Empower conversations with best-in-class communication API.
           Full-featured APIs for developers & businesses to connect and engage
           their customers globally.
@@ -232,29 +232,29 @@ const indexComp = ({ params }) => {
       </div>
 
       <div className="section c-bg-grey stats-sec align-items-center my-80">
-        <div className="container h-100 d-flex align-items-center">
-          <div className="col-3 d-flex flex-column gap-2">
+        <div className="container h-100 d-flex flex-wrap text-center text-sm-start  align-items-center">
+          <div className="col-12 col-sm-6 col-lg-3 d-flex flex-column  gap-2 py-3 py-sm-5">
             <h2 className="c-fs-1 c-fw-b">2.5 Billion+</h2>
             <h4 className="c-fs-3">
               API calls
               <br /> per month
             </h4>
           </div>
-          <div className="col-3 d-flex flex-column gap-2">
+          <div className="col-12 col-sm-6 col-lg-3 d-flex flex-column gap-2 py-3 py-sm-5">
             <h2 className="c-fs-1 c-fw-b">1.6 Billion+</h2>
             <h4 className="c-fs-3">
               SMS Sent
               <br /> per month
             </h4>
           </div>
-          <div className="col-3 d-flex flex-column gap-2">
+          <div className="col-12 col-sm-6 col-lg-3 d-flex flex-column gap-2 py-3 py-sm-5">
             <h2 className="c-fs-1 c-fw-b">1 Billion+</h2>
             <h4 className="c-fs-3">
               OTP Sent
               <br /> per month
             </h4>
           </div>
-          <div className="col-3 d-flex flex-column gap-2">
+          <div className="col-12 col-sm-6 col-lg-3 d-flex flex-column gap-2 py-3 py-sm-5">
             <h2 className="c-fs-1 c-fw-b">1 Billion+</h2>
             <h4 className="c-fs-3">
               Email Sent
@@ -266,124 +266,138 @@ const indexComp = ({ params }) => {
 
       <div className="container my-5 more-services-sec my-80">
         <h2 className="sub-heading c-head">More Services</h2>
-        <div className="d-flex my-5">
-          <div className="col-4 p-4 d-flex flex-column gap-3 b-right-1 more-services">
-            <div className="d-flex align-items-center">
-              <span className="me-3">
-                <Image
-                  src="./img/icon/shorturl.svg"
-                  width={36}
-                  height={36}
-                  alt="short-url"
-                />
-              </span>
-              <span className="c-fs-2 c-head">ShortURL</span>
-            </div>
-            <div className="">
-              <Image
-                className="more-services-img"
-                src="./img/short-url-img.svg"
-                width={336}
-                height={212}
-                alt="short-url-img"
-              />
-            </div>
-            <div className="">
-              <h4 className="mb-3 c-fs-3">Make a long story short</h4>
-              <p className="c-fs-4">
-                Track your success, one click at a time. Using our URL Shrtnr
-                App is great way to make looong, wild links into something more
-                manageable and memorable.
-              </p>
-            </div>
-            <a href={`${path}/shorturl`}>
-              <div className="d-flex align-items-center justify-content-between c-fs-3 c-fw-b text-dark">
-                <span>Learn more</span>
-                <MdKeyboardArrowRight />
+        <div className="d-flex my-5 flex-md-row flex-column">
+          <div className="col-12 col-md-4 p-4 d-flex flex-column flex-sm-row flex-md-column gap-3 b-right-1 more-services">
+            <div className="d-flex flex-column gap-3">
+              <div className="d-flex align-items-center">
+                <span className="me-3">
+                  <Image
+                    src="./img/icon/shorturl.svg"
+                    width={36}
+                    height={36}
+                    alt="short-url"
+                  />
+                </span>
+                <span className="c-fs-2 c-head">ShortURL</span>
               </div>
-            </a>
+              <div className="">
+                <Image
+                  className="more-services-img"
+                  src="./img/short-url-img.svg"
+                  width={336}
+                  height={212}
+                  alt="short-url-img"
+                />
+              </div>
+            </div>
+            <div className="d-flex flex-column gap-3 justify-content-between  h-100">
+              <div className="">
+                <h4 className="mb-3 c-fs-3">Make a long story short</h4>
+                <p className="c-fs-4">
+                  Track your success, one click at a time. Using our URL Shrtnr
+                  App is great way to make looong, wild links into something
+                  more manageable and memorable.
+                </p>
+              </div>
+              <a href={`${path}/shorturl`}>
+                <div className="d-flex align-items-center justify-content-between c-fs-3 c-fw-b text-dark">
+                  <span>Learn more</span>
+                  <MdKeyboardArrowRight />
+                </div>
+              </a>
+            </div>
           </div>
-          <div className="col-4 p-4 d-flex flex-column gap-3 b-right-1 more-services">
-            <div className="d-flex align-items-center">
-              <span className="me-3">
-                <Image
-                  src="./img/icon/knowledgebase.svg"
-                  width={36}
-                  height={36}
-                  alt="knowledgebase"
-                />
-              </span>
-              <span className="c-fs-2 c-head">Knowledgebase</span>
-            </div>
-            <div className="">
-              <Image
-                className="more-services-img"
-                src="./img/knowledgebase-img.svg"
-                width={336}
-                height={212}
-                alt="knowledgebase-img"
-              />
-            </div>
-            <div className="">
-              <h4 className="mb-3 c-fs-3">
-                Manage your help docs and articles
-              </h4>
-              <p className="c-fs-4">
-                We also offer our easy FAQ feature where your customers aren’t
-                stuck with a question ever again, providing them with instant
-                answers.
-              </p>
-            </div>
-            <a href={`${path}/knowledgebase`}>
-              <div className="d-flex align-items-center justify-content-between c-fs-3 c-fw-sb text-dark">
-                <span>Learn more</span>
-                <MdKeyboardArrowRight />
+          <div className="col-12 col-md-4 p-4 d-flex flex-column flex-sm-row flex-md-column gap-3 b-right-1  more-services">
+            <div className="d-flex flex-column gap-3">
+              <div className="d-flex align-items-center">
+                <span className="me-3">
+                  <Image
+                    src="./img/icon/knowledgebase.svg"
+                    width={36}
+                    height={36}
+                    alt="knowledgebase"
+                  />
+                </span>
+                <span className="c-fs-2 c-head">Knowledgebase</span>
               </div>
-            </a>
+              <div className="">
+                <Image
+                  className="more-services-img"
+                  src="./img/knowledgebase-img.svg"
+                  width={336}
+                  height={212}
+                  alt="knowledgebase-img"
+                />
+              </div>
+            </div>
+            <div className="d-flex flex-column gap-3 justify-content-between h-100">
+              <div className="">
+                <h4 className="mb-3 c-fs-3">
+                  Manage your help docs and articles
+                </h4>
+                <p className="c-fs-4">
+                  We also offer our easy FAQ feature where your customers aren’t
+                  stuck with a question ever again, providing them with instant
+                  answers.
+                </p>
+              </div>
+              <a href={`${path}/knowledgebase`}>
+                <div className="d-flex align-items-center justify-content-between c-fs-3 c-fw-sb text-dark">
+                  <span>Learn more</span>
+                  <MdKeyboardArrowRight />
+                </div>
+              </a>
+            </div>
           </div>
-          <div className="col-4 p-4 d-flex flex-column gap-3 more-services">
-            <div className="d-flex align-items-center">
-              <span className="me-3">
-                <Image
-                  src="./img/icon/filehosting.svg"
-                  width={36}
-                  height={36}
-                  alt="File Hosting"
-                />
-              </span>
-              <span className="c-fs-2 c-head">File Hosting</span>
-            </div>
-            <div className="">
-              <Image
-                className="more-services-img"
-                src="./img/file-hosting-img.svg"
-                width={336}
-                height={212}
-                alt="filehosting-img"
-              />
-            </div>
-            <div className="">
-              <h4 className="mb-3 c-fs-3">More storage, More possibilities</h4>
-              <p className="c-fs-4">
-                Track your success, one click at a time. Using our URL Shrtnr
-                App is great way to make looong, wild links into something more
-                manageable and memorable.
-              </p>
-            </div>
-            <a href="https://msg91.com/help/all-you-need-to-know-about-files-option-of-msg91">
-              <div className="d-flex align-items-center justify-content-between c-fs-3 c-fw-sb text-dark">
-                <span>Learn more</span>
-                <MdKeyboardArrowRight />
+          <div className="col-12 col-md-4 p-4 d-flex flex-column flex-sm-row flex-md-column gap-3 more-services">
+            <div className="d-flex flex-column gap-3">
+              <div className="d-flex align-items-center">
+                <span className="me-3">
+                  <Image
+                    src="./img/icon/filehosting.svg"
+                    width={36}
+                    height={36}
+                    alt="File Hosting"
+                  />
+                </span>
+                <span className="c-fs-2 c-head">File Hosting</span>
               </div>
-            </a>
+              <div className="">
+                <Image
+                  className="more-services-img"
+                  src="./img/file-hosting-img.svg"
+                  width={336}
+                  height={212}
+                  alt="filehosting-img"
+                />
+              </div>
+            </div>
+            <div className="d-flex flex-column gap-3 justify-content-between h-100">
+              <div className="">
+                <h4 className="mb-3 c-fs-3">
+                  More storage, More possibilities
+                </h4>
+                <p className="c-fs-4">
+                  Track your success, one click at a time. Using our URL Shrtnr
+                  App is great way to make looong, wild links into something
+                  more manageable and memorable.
+                </p>
+              </div>
+              <a href="https://msg91.com/help/all-you-need-to-know-about-files-option-of-msg91">
+                <div className="d-flex align-items-center justify-content-between c-fs-3 c-fw-sb text-dark">
+                  <span>Learn more</span>
+                  <MdKeyboardArrowRight />
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
 
       <div className="container  my-80">
         <h2 className="sub-heading c-head">Our Client Stories</h2>
-        <div className="d-flex col-12 gap-4 my-4">
-          <div className="col-6 c-hover ">
+        <div className="d-flex col-12 flex-column flex-sm-row gap-4 my-4">
+          <div className="col-12 col-md-8 col-sm-8 col-lg-6 c-hover ">
             <Image
               className="milkbasket-client-img "
               src="/img/milkbasket-client-img.png"
@@ -391,8 +405,8 @@ const indexComp = ({ params }) => {
               height={314}
               alt="milkbasket-client-img"
             />
-            <div className="c-fs-3 p-3">
-              <p className="pe-2">
+            <div className="c-fs-3 p-3 d-none d-sm-block">
+              <p className="pe-2 ">
                 Milkbasket sends regular updates to their customers’ via MSG91
                 multi-channel APIs.
               </p>
@@ -405,16 +419,16 @@ const indexComp = ({ params }) => {
               </a>
             </div>
           </div>
-          <div className="col-6 d-flex flex-column gap-4">
-            <div className="c-hover d-flex align-items-center">
+          <div className="col-12 col-sm-4  col-md-4 col-lg-6 d-flex flex-sm-column flex-row gap-2 gap-sm-3 gap-md-4">
+            <div className="c-hover d-flex align-items-center col-6 col-sm-12">
               <Image
-                className="secc-client-img "
+                className="secc-client-img col-12 col-lg-4"
                 src="/img/unacademy-client-img.png"
                 width={220}
                 height={220}
                 alt="secc-client-img"
               />
-              <div className="c-fs-3 p-3">
+              <div className=" d-none d-md-block c-fs-3 p-3">
                 <p className="pe-2">
                   Unacademy ensures seamless communication using SMS via MSG91.
                 </p>
@@ -427,15 +441,15 @@ const indexComp = ({ params }) => {
                 </a>
               </div>
             </div>
-            <div className="c-hover d-flex align-items-center">
+            <div className="c-hover d-flex align-items-center col-6 col-sm-12">
               <Image
-                className="secc-client-img "
+                className="secc-client-img col-12 col-lg-4 "
                 src="/img/ixigo-client-img.png"
                 width={220}
                 height={220}
                 alt="secc-client-img"
               />
-              <div className="c-fs-3 p-3">
+              <div className="d-none d-md-block c-fs-3 p-3">
                 <p className="pe-2">
                   Ixigo ensures seamless communication using SMS via MSG91.
                 </p>
