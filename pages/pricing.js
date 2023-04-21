04a72727309ff3cd1bd4f00e5202eae510762cb3
AshishYadav <ashish.y@growthways.io>
2023-04-21 19:18:51 +0530

    prod api

--- pages/pricing.js
+++ pages/pricing.js
@@ -31,7 +31,8 @@ const campaign = () => {
     for(;i<amountArr.length;i++){
       if (price.length <= amountArr.length) {
         //const response = await axios.get(`https://test.msg91.com/api/v5/web/fetchPricingDetails?amount=${item}&currency=INR&originCountry=${origin}&destinationCountry=${destination}`)
-        const response = await axios.get(`http://52.221.182.19/api/v5/web/fetchPricingDetails?amount=${amountArr[i]}&currency=INR&originCountry=${origin}&destinationCountry=${destination}`)
+        const response = await axios.get(`https://api.msg91.com/api/v5/web/fetchPricingDetails?amount=${item}&currency=INR&originCountry=${origin}&destinationCountry=${destination}`)
+        //const response = await axios.get(`http://52.221.182.19/api/v5/web/fetchPricingDetails?amount=${amountArr[i]}&currency=INR&originCountry=${origin}&destinationCountry=${destination}`)
         newData.push(response.data.data)
       }
     }
