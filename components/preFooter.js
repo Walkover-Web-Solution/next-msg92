820db1c8787ffe459bab703459888b774bdfda2c
piyussssh <mvps1862@gmail.com>
2023-04-21 19:03:16 +0530

    json updated all country

--- components/preFooter.js
+++ components/preFooter.js
@@ -5,9 +5,9 @@ const PreFooter =()=>{
                 <div className="py-4  container text-center d-md-flex justify-content-sm-between align-items-center flex-xl-row flex-lg-column flex-sm-column flex-md-column"> 
                     <span className="pro-heading heading">Register with us to give voice to your business</span>
                     <div className="mt-3 mt-xxl-0 d-flex flex-wrap justify-content-center gap-3">
-                        <a href="/contact-us" className="btn btn-outline-dark c-fs-4 ">Contact sales</a>
-                        <a href="/pricing" className="btn btn-outline-dark c-fs-4 ">Pricing</a>
-                        <a href="https://control.msg91.com/signup/" className="btn btn-dark c-fs-4 "> Get started</a>
+                        <a href="/contact-us" className="btn px-4 py-3 btn-outline-dark c-fs-4 ">Contact sales</a>
+                        <a href="/pricing" className="btn px-4 py-3 btn-outline-dark c-fs-4 ">Pricing</a>
+                        <a href="https://control.msg91.com/signup/" className="btn px-4 py-3 btn-dark c-fs-4 "> Get started</a>
                     </div>
                 </div>                          
             </div>
