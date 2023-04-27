9b91435e46f7f207b47d54ae076612ce03c412b2
AshishYadav <ashish.y@growthways.io>
2023-04-27 14:08:51 +0530

    prod api

--- components/pricingComp.js
+++ components/pricingComp.js
@@ -37,8 +37,8 @@ const PricingComp = ({countryCode}) => {
       let i=0;
       for(;i<amountArr.length;i++){
         if (price.length <= amountArr.length) {
-          const response = await axios.get(`https://test.msg91.com/api/v5/web/fetchPricingDetails?amount=${amountArr[i]}&currency=INR&originCountry=${origin}&destinationCountry=${destination}`)
-          //const response = await axios.get(`https://api.msg91.com/api/v5/web/fetchPricingDetails?amount=${amountArr[i]}&currency=INR&originCountry=${origin}&destinationCountry=${destination}`)
+          //const response = await axios.get(`https://test.msg91.com/api/v5/web/fetchPricingDetails?amount=${amountArr[i]}&currency=INR&originCountry=${origin}&destinationCountry=${destination}`)
+          const response = await axios.get(`https://api.msg91.com/api/v5/web/fetchPricingDetails?amount=${amountArr[i]}&currency=INR&originCountry=${origin}&destinationCountry=${destination}`)
           //const response = await axios.get(`http://52.221.182.19/api/v5/web/fetchPricingDetails?amount=${amountArr[i]}&currency=INR&originCountry=${origin}&destinationCountry=${destination}`)
           newData.push(response.data.data)
         }
