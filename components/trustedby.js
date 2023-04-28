fbe5fb1b9585f7ba417db9dc4be268dcfff84779
Ashish Yadav <ashish@walkover.in>
2023-04-28 14:31:39 +0530

    change trustedby logo

--- components/trustedby.js
+++ components/trustedby.js
@@ -5,11 +5,13 @@ const TrustedBy = () => {
         <div className="d-flex flex-column gap-3 my-md-5 my-2">
             <span className="c-fs-3 ">Trusted by<span className="c-fw-sb"> 30,000+ </span>businesses</span>
             <div className="d-flex gap-4 gap-md-5 justify-content-center">
-                <Image className="trustimage d-none d-lg-block" src="/img/trusted/ixigo.svg" width="187" height="28" alt="trustimage"/>
-                <Image className="trustimage " src="/img/trusted/unacademy.svg" width="187" height="28" alt="trustimage"/>
-                <Image className="trustimage" src="/img/trusted/dream11.svg" width="187" height="28" alt="trustimage"/>
-                <Image className="trustimage" src="/img/trusted/indeed.svg" width="187" height="28" alt="trustimage"/>
-                <Image className="trustimage d-none d-md-block" src="/img/trusted/housing.svg" width="187" height="28" alt="trustimage"/>
+                <Image className="trustimage " src="/img/trusted/razorpay.svg" width="187" height="28" alt="Razorpay"/>
+                <Image className="trustimage " src="/img/trusted/xiaomi.svg" width="187" height="28" alt="Xiaomi"/>
+                <Image className="trustimage " src="/img/trusted/unacademy.svg" width="187" height="28" alt="Unacademy"/>
+                <Image className="trustimage" src="/img/trusted/dream11.svg" width="187" height="28" alt="Dream11"/>
+                <Image className="trustimage" src="/img/trusted/indeed.svg" width="187" height="28" alt="Indeed"/>
+                {/* <Image className="trustimage d-none d-md-block" src="/img/trusted/housing.svg" width="187" height="28" alt="trustimage"/> */}
+                <Image className="trustimage d-none d-lg-block" src="/img/trusted/ixigo.svg" width="187" height="28" alt="Ixigo"/>
             </div>
         </div>
         
