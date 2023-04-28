0a62497b5491a9b674ada40b1eedea7852de45fb
Ashish Yadav <ashish@walkover.in>
2023-04-28 19:07:13 +0530

    add chat widget

--- components/head.js
+++ components/head.js
@@ -12,6 +12,16 @@ const Headcomp = (brawserPath) => {
         <script
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
+            __html: `var helloConfig = {
+              widgetToken: "1d31e",
+              hide_launcher: false
+            };`,
+          }}
+        />
+        <script type="text/javascript" onload="initChatWidget(helloConfig, 5000)" src="https://control.msg91.com/app/assets/widget/chat-widget.js"> </script> 
+        <script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
