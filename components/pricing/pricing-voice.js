b1f3ce7e68249d1de9d51a3ebcbc619095c7b6c9
piyussssh <mvps1862@gmail.com>
2023-05-23 16:52:09 +0530

    voice pricing

--- components/pricing/pricing-voice.js
+++ components/pricing/pricing-voice.js
@@ -1,117 +1,149 @@
-import { MdDone, MdClose } from "react-icons/md";
+import { MdDone, MdClose, MdKeyboardArrowRight,MdAdd } from "react-icons/md";
 import { useEffect, useState } from "react";
-const pricingCalls = ({subscriptionVoice, fetchSubscriptionVoice}) => {
+import { useRouter } from "next/router";
+
+const PricingCalls = ({ subscriptionVoice, fetchSubscriptionVoice }) => {
   const [selectedMode, setSelectedMode] = useState("Monthly");
   const [symbol, setSymbol] = useState("₹");
-  
-  const changeCurrency = (currency) => {    
-    fetchSubscriptionVoice(currency, '6');
+  const [searchValue, setSearchValue] = useState("");
+  const [displayValue, setDisplayValue] = useState("");
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const changeCurrency = (currency) => {
+    fetchSubscriptionVoice(currency, "6");
     switch (currency) {
       case "INR":
         setSymbol("₹");
         break;
       case "USD":
-      setSymbol("$");
-        break;      
+        setSymbol("$");
+        break;
       case "GBP":
         setSymbol("£");
         break;
+      default:
+        setSymbol("₹");
+        break;
     }
   };
+
+  const handleSearch = () => {
+    setDisplayValue(searchValue);
+    console.log("Search value:", searchValue);
+    fetchData();
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const headers = {
+        Authorization: {
+          authkey: process.env.Authorization_authkey,
+        },
+        // other headers if required
+      };
+
+      const response = await fetch(
+        `https://testvoice.phone91.com/public/dialplans/1?prefix=${searchValue}`,
+        {
+          headers: headers,
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+      } else {
+        throw new Error("Error fetching data");
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  var maindata = 0;
+  maindata = JSON.stringify(data, null, 2);
+  const router = useRouter();
+  const showDiv = router.asPath.includes("/in");
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      // Perform search operation
+      handleSearch();
+    }
+  };
+
   return (
     <>
-      {/* <div className="d-flex justify-content-center mb-4">
-        <select style={{width: 'fit-content'}} className="form-select me-4" aria-label="Default select example" onChange={(e)=>changeCurrency(e.target.value)}>
-          <option value="INR">INR</option>
-          <option value="USD">USD</option>
-          <option value="GBP">GBP</option>
-        </select>
-        <select style={{width: 'fit-content'}} className="form-select" aria-label="Default select example" onChange={(e)=>setSelectedMode(e.target.value)}>
-          <option value="Monthly">Monthly</option>
-          <option value="Half yearly">Half Yearly</option>
-          <option value="Yearly">Yearly</option>
-        </select>
-      </div>
-      <div className="d-flex flex-wrap flex-gap justify-content-center w-100  card-container align-items-end">
-        {subscriptionVoice?.length ?
-          subscriptionVoice?.map((item, index) => {
-            return(
-              <div key={`email-card-${index}`} className="mx-3">
-              {item.plan_amounts[0]?.plan_amount === 7500
-              ?              
-                <div className="text-center d-flex flex-column mb-4 mb-sm-0 align-items-center ">
-                  <div className="popular-chip voice c-fs-6">POPULAR</div>
-                  <div className="card-popular price-card card text-center mb-4 mb-sm-0 c-bg-grey">                  
-                    <div className="card-body">
-                      <h3 className="c-fs-3">{item.name}</h3>
-                      <h5 className="mt-2 c-fs-2 text-green">
-                        {symbol}
-                        {(selectedMode === 'Monthly') ? item.plan_amounts[2]?.plan_amount : (selectedMode === 'Half yearly') ? item.plan_amounts[1]?.plan_amount : item.plan_amounts[0]?.plan_amount}
-                        /
-                        {(selectedMode === 'Monthly') ? 'Month' : (selectedMode === 'Half yearly') ? 'Half yearly' : 'Yearly'}
-                      </h5>
-                      <p className="c-fs-5"> 
-                        {(item.plan_amounts[0]?.plan_amount === 0) ? '-' : '+18%GST'}
-                      </p>
-                      <div className="c-fs-5 mt-2">
-                        <span className="text-success c-fs-3">
-                          <MdDone />
-                        </span>
-                        {item.plan_services[0].service_credit.free_credits} for Outbound calls
-                      </div>                    
-                      <a href="https://control.msg91.com/signup/" target="_blank" className="c-fs-5 btn btn-sm w-100 btn-outline-dark mt-2">
-                        Get Started
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              :
-                <div className="card price-card voice border-0 text-center mb-4 mb-sm-0 c-bg-grey">                
-                  <div className="card-body">
-                    <h3 className="c-fs-3">{item.name}</h3>
-                    <h5 className="mt-2 c-fs-2 text-green">
-                      {symbol}
-                      {(selectedMode === 'Monthly') ? item.plan_amounts[2]?.plan_amount : (selectedMode === 'Half yearly') ? item.plan_amounts[1].plan_amount : item.plan_amounts[0].plan_amount}
-                      /
-                      {(selectedMode === 'Monthly') ? 'Month' : (selectedMode === 'Half yearly') ? 'Half yearly' : 'Yearly'}
-                    </h5>
-                    <p className="c-fs-5"> 
-                      {(item.plan_amounts[0]?.plan_amount === 0) ? '-' : '+18%GST'}
-                    </p>
-                    <div className="c-fs-5 mt-2">
-                      <span className="text-success c-fs-3">
-                        <MdDone />
-                      </span>
-                      {item.plan_services[0].service_credit.free_credits} for Outbound calls
-                    </div>                    
-                    <a href="https://control.msg91.com/signup/" target="_blank" className="c-fs-5 btn btn-sm w-100 btn-outline-dark mt-2">
-                      Get Started
-                    </a>
-                  </div>
-                </div>
-              }
+      <div>
+        {showDiv ? (
+          <div className="">
+            <div className="onlyin my-5 d-flex col-3 mx-auto">
+              <div class="input-group mb-3">
+              {/* <span className=" arrow-search-btn">
+                  <MdAdd className="arrow-search" />
+                </span> */}
+                <input
+                  className="c-fs-3 form-control"
+                  type="text"
+                  placeholder="Enter Country Code"
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                />
+                <span className=" btn btn-dark arrow-search-btn" onClick={handleSearch}>
+                  <MdKeyboardArrowRight className="arrow-search" />
+                </span>
               </div>
-            )
-          }):''
-      }
-        <div className="card price-card voice border-0 text-center mb-4 mb-sm-0 c-bg-grey">
-          <div className="card-body">
-            <h3 className="c-fs-3">CUSTOM</h3>                  
-            <p className="c-fs-5">Talk to sales for a customized plan.</p>
-            <button data-bs-toggle="modal" data-bs-target="#custom-pricing-modal" className="c-fs-5 btn btn-sm w-100 btn-outline-dark mt-2">
-              Talk to sales
-            </button>
-          </div>
-        </div>
-        
-      </div> */}
+            </div>
 
-    <div className="price-card rcs d-flex col-11 col-lg-10 flex-column mx-auto c-bg-grey p-4">
-      <h3 className="c-fs=3">Connect to our team for the customized pricing</h3>
-      <a href="/contact-us" className="btn btn-outline-dark col-6 col-sm-5 col-md-3 mx-auto mt-3">Talk to an Expert</a>
-    </div>
+            {loading ? (
+              <div>Loading...</div>
+            ) : error ? (
+              <div>Error: {error}</div>
+            ) : data ? (
+              <div>
+                {data.data.rates.map((rateObj, index) => (
+                  <div key={index}>
+                    <p>Prefix: {rateObj.prefix}</p>
+                    <p>Rate: {rateObj.rate}</p>
+                    <hr />
+                  </div>
+                ))}
+              </div>
+            ) : null}
+            <div className="nonin price-card rcs d-flex col-11 col-lg-10 flex-column mx-auto c-bg-grey p-4">
+              <h3 className="c-fs=3">
+                Connect to our team for the customized pricing
+              </h3>
+              <a
+                href="/contact-us"
+                className="btn btn-outline-dark col-6 col-sm-5 col-md-3 mx-auto mt-3"
+              >
+                Talk to an Expert
+              </a>
+            </div>
+          </div>
+        ) : (
+          <div className="nonin price-card rcs d-flex col-11 col-lg-10 flex-column mx-auto c-bg-grey p-4">
+            <h3 className="c-fs=3">
+              Connect to our team for the customized pricing
+            </h3>
+            <a
+              href="/contact-us"
+              className="btn btn-outline-dark col-6 col-sm-5 col-md-3 mx-auto mt-3"
+            >
+              Talk to an Expert
+            </a>
+          </div>
+        )}
+      </div>
     </>
   );
 };
 
-export default pricingCalls;
+export default PricingCalls;
