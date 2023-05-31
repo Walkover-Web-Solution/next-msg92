5a247b66bec1b99f59d7f52360824f2a1ba2c732
Ashish Yadav <ashish@walkover.in>
2023-05-31 16:04:48 +0530

    add pagination

--- components/pricing/pricing-voice.js
+++ components/pricing/pricing-voice.js
@@ -2,14 +2,18 @@ import { MdDone, MdClose, MdKeyboardArrowRight,MdAdd } from "react-icons/md";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-const PricingCalls = ({ subscriptionVoice, fetchSubscriptionVoice }) => {
+const PricingCalls = ({ subscriptionVoice, fetchSubscriptionVoice, countryCode }) => {
   const [selectedMode, setSelectedMode] = useState("Monthly");
   const [symbol, setSymbol] = useState("₹");
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("91");
+  const [pagination, setPagination] = useState(0);
+  const [pagesize, setPagesize] = useState(25);
+  const [pageNum, setPageNum] = useState(1);
   const [displayValue, setDisplayValue] = useState("");
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [paginationArray,setPaginationArray] = useState([]);
 
   const changeCurrency = (currency) => {
     fetchSubscriptionVoice(currency, "6");
@@ -31,14 +35,14 @@ const PricingCalls = ({ subscriptionVoice, fetchSubscriptionVoice }) => {
 
   const handleSearch = () => {
     setDisplayValue(searchValue);
-    console.log("Search value:", searchValue);
-    fetchData();
+    fetchData(1);
   };
 
-  const fetchData = async () => {
+  const fetchData = async (page) => {
+    console.log('fetch data page', page);
     setLoading(true);
     setError(null);
-
+    setPageNum(page);
     try {
       const headers = {
         Authorization: {
@@ -48,7 +52,8 @@ const PricingCalls = ({ subscriptionVoice, fetchSubscriptionVoice }) => {
       };
 
       const response = await fetch(
-        `https://testvoice.phone91.com/public/dialplans/1?prefix=${searchValue}`,
+        `https://testvoice.phone91.com/public/dialplans/1?page_size=${pagesize}&page_num=${page}&prefix=${searchValue}`,
+        //`https://control.msg91.com/api/v5/voice/public/dialplans/1?prefix=${searchValue}`,
         {
           headers: headers,
         }
@@ -57,6 +62,12 @@ const PricingCalls = ({ subscriptionVoice, fetchSubscriptionVoice }) => {
       if (response.ok) {
         const data = await response.json();
         setData(data);
+        const lenght = Math.ceil(data.data.count/pagesize)
+        console.log(lenght);
+        setPagination(lenght)
+        const d = Array.from({length: lenght}, (_, i) => i + 1)
+        console.log(d,123);
+        setPaginationArray(d)
       } else {
         throw new Error("Error fetching data");
       }
@@ -77,10 +88,84 @@ const PricingCalls = ({ subscriptionVoice, fetchSubscriptionVoice }) => {
     }
   };
 
+  useEffect(() => {
+    fetchData(1);    
+  }, [countryCode]);
+
   return (
     <>
-      <div>
-      
+        {showDiv ? (
+          <>
+            <div className="onlyin mb-5 d-flex col-3 mx-auto">
+              <div className="input-group mb-3">
+              {/* <span className=" arrow-search-btn">
+                  <MdAdd className="arrow-search" />
+                </span> */}
+                <input
+                  className="c-fs-3 form-control"
+                  type="text"
+                  placeholder="Enter Country Code"
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                />
+                <span className=" btn btn-dark arrow-search-btn" onClick={handleSearch}>
+                  <MdKeyboardArrowRight className="arrow-search" />
+                </span>
+              </div>
+            </div>
+
+            {loading ? (
+              <div>Loading...</div>
+            ) : error ? (
+              <div>Error: {error}</div>
+            ) : data ? (
+                <div className="m-auto rate-table">
+                  <table className="table">
+                    <thead>
+                      <tr>                      
+                        <th scope="col">Prefix</th>
+                        <th scope="col">Rate</th>                      
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {data.data.rates.map((rateObj, index) => (
+                      <tr key={`rate-${index}`}>                    
+                        <td>{rateObj.prefix}</td>
+                        <td>{rateObj.rate} <small>{data.data.currency_code}</small></td>
+                      </tr>                    
+                    ))}
+                    </tbody>                  
+                  </table>
+                  <div className="pb-5"></div>                  
+                  <div className="mb-5 d-flex">
+                    <ul className="pagination m-auto">
+                      {pagination > 1 && 
+                        paginationArray.map((num)=>{
+                          return(
+                            <li className={`page-item ${ pageNum === num ? 'active' : ''}`} key={`page-${num}`} onClick={()=>fetchData(num)}>
+                              <a class="page-link">{num}</a>
+                            </li>  
+                          )
+                        })
+                        }           
+                    </ul>
+                  </div>
+                </div>
+            ) : null}
+            <div className="nonin price-card rcs d-flex col-11 col-lg-10 flex-column mx-auto c-bg-grey p-4">
+              <h3 className="c-fs=3">
+                Connect to our team for the customized pricing
+              </h3>
+              <a
+                href="/contact-us"
+                className="btn btn-outline-dark col-6 col-sm-5 col-md-3 mx-auto mt-3"
+              >
+                Talk to an Expert
+              </a>
+            </div>
+          </>
+        ) : (
           <div className="nonin price-card rcs d-flex col-11 col-lg-10 flex-column mx-auto c-bg-grey p-4">
             <h3 className="c-fs=3">
               Connect to our team for the customized pricing
@@ -92,8 +177,7 @@ const PricingCalls = ({ subscriptionVoice, fetchSubscriptionVoice }) => {
               Talk to an Expert
             </a>
           </div>
-        
-      </div>
+        )}
     </>
   );
 };
