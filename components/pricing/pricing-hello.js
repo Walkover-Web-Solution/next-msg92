8a6f667c308039dfc70e6be9d677a1fac0d089d6
piyussssh <mvps1862@gmail.com>
2023-06-28 16:05:19 +0530

    hello pricing comment

--- components/pricing/pricing-hello.js
+++ components/pricing/pricing-hello.js
@@ -1,3 +1,4 @@
+import {MdDoneOutline} from "react-icons/md"
 const pricinghello = ({ countryCode }) => {
   const notavailable = "/img/not-available-red.svg";
   const available = "/img/available-green.svg";
@@ -19,7 +20,23 @@ const pricinghello = ({ countryCode }) => {
   // console.log(basicRate,countryCode)
   return (
     <>
-      <div className="container border">
+
+<div className="price-card hello d-flex col-10 col-lg-11 flex-md-row flex-column mx-auto border">
+    <div className="col-12 col-md-8 col-xl-9 text-start p-4 bg-white">
+      <h3>Customer support Platform</h3>
+      <div className="d-flex"><div className="c-fs-4 text-primary pe-2"><MdDoneOutline/></div><p className="c-fs-4">Integrate multiple channels on a single platform - Email, RCS, social media, call & more</p></div>
+      <div className="d-flex mt-2"><div className="c-fs-4 text-primary pe-2"><MdDoneOutline/></div><p className="c-fs-4">Track, monitor, analyze agent performance</p></div>
+      <div className="d-flex mt-2"><div className="c-fs-4 text-primary pe-2"><MdDoneOutline/></div><p className="c-fs-4">18*7 free customer support</p></div>
+    </div>
+    <div className="c-bg-grey col-12 col-md-4 col-xl-3  p-4 text-start">
+      <h3 className="c-fs-3">Access platform for FREE</h3>
+      <p className="c-fs-4 mt-2">Pay only for the channels you use</p>
+      <a href="https://control.msg91.com/signup/" target="_blank" className="btn btn-outline-dark mt-3 px-5">Get Started</a>
+    </div>
+    </div>
+
+
+      {/* <div className="container border">
         <div className="column text-start">
           <div className="row c-head c-fs-3 py-2  border-bottom">
             <div className="col-4"></div>
@@ -328,7 +345,7 @@ const pricinghello = ({ countryCode }) => {
             prepaid and prorated. He has to pay for the inbox whenever he wants
             to create or enable an inbox
           </p>
-        </div>
+        </div> */}
     </>
   );
 };
