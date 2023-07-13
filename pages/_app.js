5b87c168b346c46aa21fae631cf179e7d9e8831a
Ashish Yadav <ashish@walkover.in>
2023-07-13 11:52:44 +0530

    extra param issue

--- pages/_app.js
+++ pages/_app.js
@@ -10,9 +10,13 @@ export default function App({ Component, pageProps }) {
   const router = useRouter();
   // const { slug } = router.query; 
   var  brawserPath = router.asPath;
+  const pattern = /\/([^/?]+)/;
+  const result = brawserPath.match(pattern);  
+  brawserPath = result[0];
   var path = brawserPath.split("/")[1];
-  const year = new Date().getFullYear();
+  //console.log(brawserPath, path, router);
 
+  const year = new Date().getFullYear();
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
