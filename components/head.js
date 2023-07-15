f96e245a7459cba0244359313c3389c93607feb8
Ashish Yadav <ashish@walkover.in>
2023-07-15 17:00:45 +0530

    change meta data

--- components/head.js
+++ components/head.js
@@ -36,7 +36,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         <title>{meta?.title}</title>
-        <meta name="discription" content={meta?.description} />
+        <meta name="description" content={meta?.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="google-site-verification" content="RfcBy_Lv1Ao1j0eP8UlMjJ44ik5_1YDKsRQSNFr9jEQ" />
         <link rel="icon" href="/fav.svg" />
