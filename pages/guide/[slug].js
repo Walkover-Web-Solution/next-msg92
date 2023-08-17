19d39995e14a5e1ec5f8a7f21e8b221a32139b95
Ashish Yadav <ashish@walkover.in>
2023-08-17 11:11:36 +0530

    blog config change:8

--- pages/guide/[slug].js
+++ pages/guide/[slug].js
@@ -1,17 +1,11 @@
-import { serialize } from 'next-mdx-remote/serialize';
-import { MDXRemote } from 'next-mdx-remote';
- 
-export default function Post({ source }) {
-  return (
-    <div className="wrapper">
-      <MDXRemote {...source} />
-    </div>
-  );
-}
- 
-export async function getStaticProps({}) {
-  // MDX text - can be from a local file, database, anywhere
-  const source = 'Some **mdx** text, with a component';
-  const mdxSource = await serialize(source);
-  return { props: { source: mdxSource } };
-}
+
+const page = () => {
+    return (
+      <>
+          <h1>Guide</h1>  
+      </>
+    );
+  };
+  
+  export default page;
+  
