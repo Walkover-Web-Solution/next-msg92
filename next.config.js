2f238ed71c45a1b110ba4e746c2ccd033655ab02
shubhamkeshari <66933373+shubhamkeshari@users.noreply.github.com>
2023-08-19 18:28:58 +0530

    email login flow set

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
+  env: {
+    API_BASE_URL: 'https://test.msg91.com',
+    firebaseConfig: {
+      apiKey: "AIzaSyCxRjom_yz_suroteRqwqBk91HPqgh4Z3c",
+      authDomain: "admin-panel-test-b5a7e.firebaseapp.com",
+      projectId: "admin-panel-test-b5a7e",
+      storageBucket: "admin-panel-test-b5a7e.appspot.com",
+      messagingSenderId: "970692014254",
+      appId: "1:970692014254:web:a50174f6d1d939533f7d3f",
+    },
+    zohoClientId: "1000.ZD8TKFCPWAYEIX1IM4FJ8F9LRMNYEB",
+    githubClientId: "b7432f8d5a708e33b3b0",
+    msalClientId: "164d09e1-531c-427d-9358-9d1347c74170",
+    msalTenantId: "f8cdef31-a31e-4b4a-93e4-5f571e91255a",
+    redirectURL: "http://localhost:9999",
+    OTPWidgetToken: "3366766c6f67373339303538",
+    widgetAuthToken: "278060TvhhI9M8s64ce1109P1",
+    widgetscript:
+      "https://test.msg91.com/hello-new/assets/otp-provider/otp-provider.js",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/login.js
+++ pages/login.js
@@ -1,86 +1,177 @@
-const logIn = () => {
-  console.log("inside login page");
-    return (
-      <>
-        <section className="entry d-flex  ">
-          <div className="entry__left_section col-xl-3 col-lg-5 col-md-5">  
-            <img
-              src="/images/msgOriginalsvg.png"
-              className="entry__left_section__brand_logo"
-            />
-            <div className="container">
-            <img
-              src="/img/tie/login-page-left-section-banner.svg"
-              className="entry__left_section__banner pe-4 img-fluid"
-            />
-            </div>
-          </div>
-          <div className="entry__right_section col-xl-9 col-lg-8 col-md-7 col-sm-12 col-12">
-            <div className="container entry__right_section__container">
-              <div className="d-none mb-5 entry__right_section__container--logo-visible-in-small">
-                <img
-                  src="/images/msgOriginalsvg.png"
-                  width="auto"
-                  height="40px"
-                  alt="MSG91"
-                  className=""
-                />
-              </div>
-              <h1>Welcome back!</h1>
-              <div className="entry__right_section__container__entry_with mb-4">
-                <span className="d-inline-block mt-3 p-2 mb-3">Login with</span>
-  
-                <div className="d-flex align-items-center flex-wrap login-icon-cont" style={{gap: "16px"}}>
-                  <button className="entry__right_section__container__entry_with--btn-with-text" style={{border: "1px solid var(--primary-light-theme, #1E75BA)"}}>
-                    <img
-                      src="/img/tie/google-logo.svg"
-                      alt="Google Icon"
-                      />
-                      <span>Google</span>
-                      </button>
-                    <button style={{border: "1px solid var(--primary-light-theme, #1E75BA)", background: "var(--light-white-bg, #FFF)"}}>
-                    <img 
-                      src="/img/microsoft-svg.svg"
-                    />
-                    </button>
-                    <button style={{border: "1px solid #D94C44"}}>
-                    <img
-                    src="/img/icon-zogo.svg"
-                    />
-                    </button>
-                    <button style={{border:"1px solid #000", background: "var(--light-white-bg, #FFF)"}}>
-                    <img 
-                    src="/img/icon-github.svg"
-                    />
-                  </button>
-                    <button style={{border: "1px solid #E24329", background: "var(--light-white-bg, #FFF)"}}>
-                    <img 
-                    src="/img/icon-gitlab.svg"
-                    />
-                    </button>
-                    
-                  <button style={{border:"1px solid #2684FF", background: "var(--light-white-bg, #FFF)"}}>
-                    <img
-                    src="/img/icon-group.svg"
-                    />
-                  </button>
-                </div>
-              </div>
-  
-              <span className="d-block line_on_right c-fs-6 mb-4">or</span>
-  
-              <button className="c-col-white entry__right_section__container__entry_button mb-4">
-                Login with OTP
-              </button>
-  
-              <p className="c-fs-6 mb-4">
-                Trouble logging in ? <a href="#" className="text_blue">Click here</a>
-              </p>
-              <a href="#" className="c-fs-6 text_blue">Create new account</a>
-            </div>
-          </div>
-        </section>
-      </>
-    );
-  };
-  export default logIn;
+import React from "react";
+
+class logIn extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loginInProgress: false
+        };
+    }
+
+    componentDidMount() {
+        this.otpWidgetSetup();
+    }
+
+    otpWidgetSetup = () => {
+        const head = document.getElementsByTagName("head")[0];
+        const currentTimestamp = new Date().getTime();
+        const otpWidgetScript = document.createElement("script");
+        otpWidgetScript.type = "text/javascript";
+        console.log(process.env.widgetscript, process.env.OTPWidgetToken);
+        otpWidgetScript.src = `${process.env.widgetscript}?v=${currentTimestamp}`;
+        otpWidgetScript.onload = () => {
+            console.log("loaded");
+        };
+        head.appendChild(otpWidgetScript);
+    };
+
+    initOTPWidget() {
+        const configuration = {
+            widgetId: process.env.OTPWidgetToken,
+            tokenAuth: process.env.widgetAuthToken,
+            success: (data) => {
+                // Widget config success response
+                try {
+                    const url = process.env.API_BASE_URL + "/api/v5/nexus/emailLogin";
+                    const requestOptions = {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({ code: data.message })
+                    };
+                    fetch(url, requestOptions)
+                        .then(response => response.json())
+                        .then(result => {
+                            console.log(result);
+                            // this.loginCompleted()
+                        });
+                } catch (error) {
+                    loginFailed(error);
+                }
+            },
+            failure: (error) => {
+                // Widget config failure response
+                loginFailed(error);
+            },
+        };
+        window.initSendOTP(configuration);
+    }
+
+    loginCompleted() {
+        location.href = process.env.API_BASE_URL + '/app/'
+    }
+
+    loginFailed(error) {
+        console.log(error);
+    }
+
+    render() {
+        return (
+            <>
+                <section className="entry d-flex  ">
+                    <div className="entry__left_section col-xl-3 col-lg-5 col-md-5">
+                        <img
+                            src="/images/msgOriginalsvg.png"
+                            className="entry__left_section__brand_logo"
+                        />
+                        <div className="container">
+                            <img
+                                src="/img/tie/login-page-left-section-banner.svg"
+                                className="entry__left_section__banner pe-4 img-fluid"
+                            />
+                        </div>
+                    </div>
+                    <div className="entry__right_section col-xl-9 col-lg-8 col-md-7 col-sm-12 col-12">
+                        <div className="container entry__right_section__container">
+                            <div className="d-none mb-5 entry__right_section__container--logo-visible-in-small">
+                                <img
+                                    src="/images/msgOriginalsvg.png"
+                                    width="auto"
+                                    height="40px"
+                                    alt="MSG91"
+                                    className=""
+                                />
+                            </div>
+                            <h1>Welcome back!</h1>
+                            <div className="entry__right_section__container__entry_with mb-4">
+                                <span className="d-inline-block mt-3 p-2 mb-3">Login with</span>
+
+                                <div
+                                    className="d-flex align-items-center flex-wrap login-icon-cont"
+                                    style={{ gap: "16px" }}
+                                >
+                                    <button
+                                        className="entry__right_section__container__entry_with--btn-with-text"
+                                        style={{
+                                            border: "1px solid var(--primary-light-theme, #1E75BA)",
+                                        }}
+                                    >
+                                        <img src="/img/tie/google-logo.svg" alt="Google Icon" />
+                                        <span>Google</span>
+                                    </button>
+                                    <button
+                                        style={{
+                                            border: "1px solid var(--primary-light-theme, #1E75BA)",
+                                            background: "var(--light-white-bg, #FFF)",
+                                        }}
+                                    >
+                                        <img src="/img/microsoft-svg.svg" />
+                                    </button>
+                                    <button style={{ border: "1px solid #D94C44" }}>
+                                        <img src="/img/icon-zogo.svg" />
+                                    </button>
+                                    <button
+                                        style={{
+                                            border: "1px solid #000",
+                                            background: "var(--light-white-bg, #FFF)",
+                                        }}
+                                    >
+                                        <img src="/img/icon-github.svg" />
+                                    </button>
+
+                                    {/* <button
+                                        style={{
+                                            border: "1px solid #E24329",
+                                            background: "var(--light-white-bg, #FFF)",
+                                        }}
+                                    >
+                                        <img src="/img/icon-gitlab.svg" />
+                                    </button> */}
+
+                                    {/* <button
+                                        style={{
+                                            border: "1px solid #2684FF",
+                                            background: "var(--light-white-bg, #FFF)",
+                                        }}
+                                    >
+                                        <img src="/img/icon-group.svg" />
+                                    </button> */}
+                                </div>
+                            </div>
+
+                            <span className="d-block line_on_right c-fs-6 mb-4">or</span>
+
+                            <button className="c-col-white entry__right_section__container__entry_button mb-4" onClick={() => this.initOTPWidget()}>
+                                Login with OTP
+                            </button>
+
+                            <p className="c-fs-6 mb-4">
+                                Trouble logging in ?{" "}
+                                <a href="#" className="text_blue">
+                                    Click here
+                                </a>
+
+                                {/* https://control.msg91.com/signin/ */}
+                            </p>
+                            <a href="/signup" className="c-fs-6 text_blue">
+                                Create new account
+                            </a>
+                        </div>
+                    </div>
+                </section>
+            </>
+        );
+    }
+}
+export default logIn;
