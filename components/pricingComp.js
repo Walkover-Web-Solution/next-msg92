c5ed0e1b4023aa77a4b0b976620e5ce1950d8870
Ashish Yadav <ashish@walkover>
2023-09-07 15:51:53 +0530

    change prod api

--- components/pricingComp.js
+++ components/pricingComp.js
@@ -118,13 +118,13 @@ const PricingComp = ({ countryCode, product, brawserPath }) => {
     changeCurrencySymbol(currency);
     try {
       const fetchRequests = amountArr.map(async (amount) => {
-        /* const response = await axios.get(
-          `https://api.msg91.com/api/v5/web/fetchPricingDetails?amount=${amount}&currency=${currency}&originCountry=${origin}&destinationCountry=${destination}`
-          ) */
-
         const response = await axios.get(
+          `https://api.msg91.com/api/v5/web/fetchPricingDetails?amount=${amount}&currency=${currency}&originCountry=${origin}&destinationCountry=${destination}`
+          )
+
+        /* const response = await axios.get(
           `https://test.msg91.com/api/v5/web/fetchPricingDetails?amount=${amount}&currency=${currency}&originCountry=${origin}&destinationCountry=${destination}`
-        );
+        ); */
         return response.data.data;
       });
   
