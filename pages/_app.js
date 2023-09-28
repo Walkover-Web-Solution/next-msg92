5e9cc9531eeb5d2a3933d51249d7e9bcae73f228
Ashish Yadav <ashish@walkover>
2023-09-28 18:01:49 +0530

    add widget page

--- pages/_app.js
+++ pages/_app.js
@@ -88,7 +88,7 @@ export default function App({ Component, pageProps }) {
           src="https://control.msg91.com/app/assets/widget/chat-widget.js"
         />
 
-        <Script
+        {/* <Script
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -97,7 +97,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NWZKLRJ');`,
           }}
-        />
+        /> */}
 
         { browserPath.browserPath == '/in' &&
           <>
