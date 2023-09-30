0c1f369ee749d5455eeb2903dc70dadb0377dbb3
Ashish Yadav <ashish@walkover>
2023-09-30 11:30:57 +0530

    uncomment gtm code

--- pages/_app.js
+++ pages/_app.js
@@ -80,16 +80,16 @@ export default function App({ Component, pageProps }) {
                 src="https://control.msg91.com/app/assets/widget/chat-widget.js"
             />
 
-        {/* <Script
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NWZKLRJ');`,
-          }}
-        /> */}
+            <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NWZKLRJ');`,
+            }}
+            />
 
             {browserPath.browserPath == '/in' && (
                 <>
