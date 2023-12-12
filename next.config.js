6864ca736661eeb17ff79dfe0c6369af291a4c1c
Rahul Choudhary <rahulchoudhary@walkover.in>
2023-12-12 16:33:19 +0530

    Fix: `.env` not working for local

--- next.config.js
+++ next.config.js
@@ -1,23 +1,26 @@
 /** @type {import('next').NextConfig} */
 
+var customEnvConfig = require('dotenv')?.config()?.parsed || {};
+
+var getEnvValue = (key) => process.env?.[key] || customEnvConfig?.[key];
 
 const nextConfig = {
     reactStrictMode: false,
     env: {
         // URLS
-        API_BASE_URL: process.env.API_BASE_URL,
-        REDIRECT_URL: process.env.REDIRECT_URL,
+        API_BASE_URL: getEnvValue('API_BASE_URL'),
+        REDIRECT_URL: getEnvValue('REDIRECT_URL'),
 
-        // CLIENT IDS 
-        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-        ZOHO_CLIENT_ID: process.env.ZOHO_CLIENT_ID,
-        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-        MSAL_CLIENT_ID: process.env.MSAL_CLIENT_ID,
+        // CLIENT IDS
+        GOOGLE_CLIENT_ID: getEnvValue('GOOGLE_CLIENT_ID'),
+        ZOHO_CLIENT_ID: getEnvValue('ZOHO_CLIENT_ID'),
+        GITHUB_CLIENT_ID: getEnvValue('GITHUB_CLIENT_ID'),
+        MSAL_CLIENT_ID: getEnvValue('MSAL_CLIENT_ID'),
 
         //WIDGET CONFIG
-        OTP_WIDGET_TOKEN: process.env.OTP_WIDGET_TOKEN,
-        WIDGET_AUTH_TOKEN: process.env.WIDGET_AUTH_TOKEN,
-        WIDGET_SCRIPT: process.env.WIDGET_SCRIPT,
+        OTP_WIDGET_TOKEN: getEnvValue('OTP_WIDGET_TOKEN'),
+        WIDGET_AUTH_TOKEN: getEnvValue('WIDGET_AUTH_TOKEN'),
+        WIDGET_SCRIPT: getEnvValue('WIDGET_SCRIPT'),
     },
 
     // Can be safely removed in newer versions of Next.js
