782eaed327b83a45e85cd0431684afea5a36496d
Rahul Choudhary <rahulchoudhary@walkover.in>
2023-12-13 12:44:22 +0530

    build fix

--- components/postItem.js
+++ components/postItem.js
@@ -58,8 +58,6 @@ export default function PostItem({ post }) {
         </div>
       </div>
     </a>
-    {/* {content && <button className="message" onClick={()=>showToaster("not valid ", "error")}>click me</button>} */}
-    </>
   );
 
 }
