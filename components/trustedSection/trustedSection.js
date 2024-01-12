12ad5b591017d3e307c60c29e62acbe012600656
piyusssshh <piyush@whozzat.com>
2024-01-12 19:05:30 +0530

    new hello > main

--- components/trustedSection/trustedSection.js
+++ components/trustedSection/trustedSection.js
@@ -7,7 +7,7 @@ export default function TrusedSection(trustedData) {
         <p className={`${styles.heading} c-fs-3`}>Support 15+ channels & platforms</p>
         <div className={`${styles.imgs} d-flex flex-wrap gap-3 gap-lg-4`}>
           {trustedData?.trustedData?.trustedImgs.map((data, index) => (
-            <img key={index} className={`${styles.img}`} src={`./img/pages/hello/platforms/${data}.svg`} alt={data} />
+            <img key={index} className={`${styles.img}`} src={`/img/pages/hello/platforms/${data}.svg`} alt={data} />
           ))}
           {trustedData?.trustedData?.moreText && (
             <span  className={`${styles.moretext}`}>{trustedData?.trustedData?.moreText}</span>
