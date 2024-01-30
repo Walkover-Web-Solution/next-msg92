f36a39ad6cf9379f30c3c03c5707a13c50c7179a
Rahul Choudhary <rahulchoudhary@walkover.in>
2024-01-30 17:00:43 +0530

    added nullity check

--- components/notification.js
+++ components/notification.js
@@ -11,7 +11,7 @@ const Notification = () => {
     var cookie = getCookie("country_code");
     for (let x in countries) {
       path = cookie;
-      if (path.toUpperCase() === countries[x].sortname) {
+      if (path?.toUpperCase() === countries[x].sortname) {
         setCountry(countries[x].name);
         break;
       }
