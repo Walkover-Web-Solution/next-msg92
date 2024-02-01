ec4eec31d01ecc44661da66271f90d297185b992
Saurabh186 <sawrabhsharma1234@gmail.com>
2024-02-01 16:20:31 +0530

    navbar button rounded

--- components/navbar.js
+++ components/navbar.js
@@ -150,7 +150,7 @@ const navbar = ({ browserPath, pricingPath,pageSlug ,cc}) => {
             <Link
               href="https://control.msg91.com/signup/"
               target="_blank"
-              className="btn btn-dark c-fs-4 c-fw-sb px-3 utm"
+              className="btn btn-dark c-fs-4 c-fw-sb px-3 utm rounded-1"
               id="signup-btn"
             >
               Sign Up
