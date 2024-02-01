11565f84a735a99f039e2c3720bd76a5df3e9918
Saurabh186 <sawrabhsharma1234@gmail.com>
2024-02-01 17:39:11 +0530

    font size and button rounded

--- components/getStartedSection/getStartedSection.js
+++ components/getStartedSection/getStartedSection.js
@@ -3,25 +3,25 @@ export default function GetStartedSection({ pricingPath }) {
     <>
       <div className="c-bg-grey p-5">
         <div className="container d-flex flex-column flex-md-row gap-4 align-items-center justify-content-between">
-          <span className=" c-fs-3 c-fw-b">
+          <span className="fw-bold small-heading">
             Start building your ideal customer engagement experience{" "}
           </span>
           <div className=" d-flex row  gap-3">
               <a
                 href="/contact-us"
-                className="btn btn-ft px-3 py-2 btn-outline-dark c-fs-4 col "
+                className="btn btn-ft px-3 py-2 btn-outline-dark c-fs-3 col rounded-1 c-fw-sb"
               >
                 Contact sales
               </a>
               <a
                 href={pricingPath}
-                className="btn btn-ft px-3 py-2 btn-outline-dark c-fs-4 col"
+                className="btn btn-ft px-3 py-2 btn-outline-dark c-fs-3 col rounded-1 c-fw-sb"
               >
                 Pricing
               </a>
             <a
               href="https://control.msg91.com/signup/"
-              className="btn btn-ft px-3 py-2 btn-dark c-fs-4 utm col-12 col-md w-100"
+              className="btn btn-ft px-3 py-2 btn-dark c-fs-3 utm col-12 col-lg  w-100 rounded-1 c-fw-sb"
             >
               Get started
             </a>
