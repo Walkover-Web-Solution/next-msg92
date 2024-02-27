a0460d5e675902b4c7bf00fac144f6ea081b5f3d
Saurabh186 <sawrabhsharma1234@gmail.com>
2024-02-27 16:19:41 +0530

    SAURABH | FIX accessibilityd all footer  | ul tag added

--- components/termsofuse/no-spam.js
+++ components/termsofuse/no-spam.js
@@ -4,7 +4,7 @@ const nospam = () => {
             <div className="mb-5">
                 <h1 className="c-fs-2 c-fw-m">No spam policy</h1>
                 <span className="c-fs-4">
-                We don’t SPAM and won’t accept anyone’s either!
+                    We don’t SPAM and won’t accept anyone’s either!
                     <div className="my-3"></div>
                     Any unsolicited, unexpected or unwanted SMS sent to the recipients in order to mislead them is considered SPAM by us. Also, if you send messages to unregistered users- SPAM.
                 </span>
@@ -12,7 +12,7 @@ const nospam = () => {
             <div className="mb-5">
                 <h1 className="c-fs-2 c-fw-m">MSG91’s No-Spam Policy</h1>
                 <span className="c-fs-4">
-                General conditions
+                    General conditions
                     <div className="my-3"></div>
                     We consider SMS to be a compelling tool for delivering useful information, alerts and notifications.
                     <div className="my-3"></div>
@@ -24,27 +24,33 @@ const nospam = () => {
                     <div className="my-3"></div>
                     Conditions specific to Promotional Route/R1
                     <div className="my-3"></div>
-                    <li>
-                    Promotional SMS will only be processed between 9AM to 9PM (respective country time-zone). MSG91 reserves the right to reject or store in queue the SMS submitted after 9PM to be processed in route 1 working hours.
-                    </li>
-                    <li>
-                    Sender IDs are subjected to availability in recipient’s country.
-                    </li>
-                    <li>
-                    If the national telecom regulatory of a country provides Do Not Disturb (DND) number database, then promotional SMS’s will not be delivered on these (DND) numbers.
-                    </li>
+                    <ul>
+                        <li>
+                            Promotional SMS will only be processed between 9AM to 9PM (respective country time-zone). MSG91 reserves the right to reject or store in queue the SMS submitted after 9PM to be processed in route 1 working hours.
+                        </li>
+                        <li>
+                            Sender IDs are subjected to availability in recipient’s country.
+                        </li>
+                        <li>
+                            If the national telecom regulatory of a country provides Do Not Disturb (DND) number database, then promotional SMS’s will not be delivered on these (DND) numbers.
+                        </li>
+                    </ul>
+
                     <div className="my-3"></div>
                     Conditions specific to Transactional Route/R4
                     <div className="my-3"></div>
-                    <li>
-                    Telecom regularities may ask to register yourself before you send SMS to a country.
-                    </li>
-                    <li>
-                    If a valid complaint is filed against you, Monetary fine may be implied (subject to laws constituted by regulatory authority.)
-                    </li>
-                    <li>
-                    If system algorithm filters an SMS as promotional, manual review is possible.
-                    </li>
+                    <ul>
+
+                        <li>
+                            Telecom regularities may ask to register yourself before you send SMS to a country.
+                        </li>
+                        <li>
+                            If a valid complaint is filed against you, Monetary fine may be implied (subject to laws constituted by regulatory authority.)
+                        </li>
+                        <li>
+                            If system algorithm filters an SMS as promotional, manual review is possible.
+                        </li>
+                    </ul>
                     <div className="my-3"></div>
                     Content that is prohibited.
                     <div className="my-3"></div>
@@ -52,74 +58,80 @@ const nospam = () => {
                     <div className="my-3"></div>
                     We do not send:
                     <div className="my-3"></div>
-                    <li>
-                    SMS that violate CAN-SPAM Act;
-                    </li>
-                    <li>
-                    Sexually explicit messages;
-                    </li>
-                    <li>
-                    Marketing SMS to a list of people without their permission (unsolicited commercial SMS.) We review each profile to make sure it meets our terms of service, which help us maintain deliverability for all of our clients.
-                    </li>
+                    <ul>
+                        <li>
+                            SMS that violate CAN-SPAM Act;
+                        </li>
+                        <li>
+                            Sexually explicit messages;
+                        </li>
+                        <li>
+                            Marketing SMS to a list of people without their permission (unsolicited commercial SMS.) We review each profile to make sure it meets our terms of service, which help us maintain deliverability for all of our clients.
+                        </li>
+                    </ul>
                     <div className="my-3"></div>
                     Please understand that some industries or “niches” return more abuse and spam complaints than average. We cannot risk our server’s reputation and connectivity with operators, which affects each users profile. Our service does not accept SMS campaigns, that offer types of services, products, or content listed below:
                     <div className="my-3"></div>
-                    <li>
-                    Escort and dating services;
-                    </li>
-                    <li>
-                    Lottery services;
-                    </li>
-                    <li>
-                    Online and direct pharmaceutical sales, including but not limited to health and sexual well-being products,
-                    </li>
-                    <li>
-                    prescription and counterfeit drugs;
-                    </li>
-                    <li>
-                    Work from home, make money online, and lead generation opportunities (“get rich quick”, “build your wealth”, “financial independence”);
-                    </li>
-                    <li>
-                    Pyramid schemes or multi-level channel, network and/or referral marketing (MLM) businesses used for prospecting or recruiting;
-                    </li>
-                    <li>
-                    Affiliate marketing;
-                    </li>
-                    <li>
-                    Debt collections, credit repair and debt relief offerings;
-                    </li>
-                    <li>
-                    Mortgages and loans;
-                    </li>
-                    <li>
-                    Nutritional, herbal, and vitamin supplements;
-                    </li>
-                    <li>
-                    Adult novelty items or references;
-                    </li>
-                    <li>
-                    List brokers or list rental services;
-                    </li>
-                    <li>
-                    Illegal goods, pirated software or media;
-                    </li>
-                    <li>
-                    Odds making and betting/gambling services, including but not limited online poker or casino games.
-                    </li>
+                    <ul>
+                        <li>
+                            Escort and dating services;
+                        </li>
+                        <li>
+                            Lottery services;
+                        </li>
+                        <li>
+                            Online and direct pharmaceutical sales, including but not limited to health and sexual well-being products,
+                        </li>
+                        <li>
+                            prescription and counterfeit drugs;
+                        </li>
+                        <li>
+                            Work from home, make money online, and lead generation opportunities (“get rich quick”, “build your wealth”, “financial independence”);
+                        </li>
+                        <li>
+                            Pyramid schemes or multi-level channel, network and/or referral marketing (MLM) businesses used for prospecting or recruiting;
+                        </li>
+                        <li>
+                            Affiliate marketing;
+                        </li>
+                        <li>
+                            Debt collections, credit repair and debt relief offerings;
+                        </li>
+                        <li>
+                            Mortgages and loans;
+                        </li>
+                        <li>
+                            Nutritional, herbal, and vitamin supplements;
+                        </li>
+                        <li>
+                            Adult novelty items or references;
+                        </li>
+                        <li>
+                            List brokers or list rental services;
+                        </li>
+                        <li>
+                            Illegal goods, pirated software or media;
+                        </li>
+                        <li>
+                            Odds making and betting/gambling services, including but not limited online poker or casino games.
+                        </li>
+                    </ul>
                     <div className="my-3"></div>
                     Our Definition of Illegally Collected Number Lists
                     <div className="my-3"></div>
                     A number list is legit if the recipient agreed to receive SMS from you.
                     <div className="my-3"></div>
-                    <li>
-                    Renting/buying numbers from a third-party.
-                    </li>
-                    <li>
-                    Collecting from Yellow Pages, online directories, etc.
-                    </li>
-                    <li>
-                    Getting numbers with the help of number extracting softwares.
-                    </li>
+                    <ul>
+                        <li>
+                            Renting/buying numbers from a third-party.
+                        </li>
+                        <li>
+                            Collecting from Yellow Pages, online directories, etc.
+                        </li>
+                        <li>
+                            Getting numbers with the help of number extracting softwares.
+                        </li>
+                    </ul>
                     <div className="my-3"></div>
                     MSG91 SMTP doesn’t allow SPAM messages to be sent to a mobile number from our service.
                     <div className="my-3"></div>
@@ -129,21 +141,23 @@ const nospam = () => {
                     <div className="my-3"></div>
                     SMS must comply with the following principles:
                     <div className="my-3"></div>
-                    <li>
-                    No false, invalid or misleading information in the body of SMS should be sent using MSG91 Service.
-                    </li>
-                    <li>
-                    No SMS must be sent to recipients without having previously obtained her/his consent.
-                    </li>
-                    <li>
-                    Text displayed must clearly state or illustrate the service that is offered.
-                    </li>
-                    <li>
-                    Marketing campaigns must clearly identify who is providing the service (originator) and clearly state the expense of the service promoted.
-                    </li>
-                    <li>
-                    It is strictly forbidden to send messages to illegally collected numbers via MSG91 servers.
-                    </li>
+                    <ul>
+                        <li>
+                            No false, invalid or misleading information in the body of SMS should be sent using MSG91 Service.
+                        </li>
+                        <li>
+                            No SMS must be sent to recipients without having previously obtained her/his consent.
+                        </li>
+                        <li>
+                            Text displayed must clearly state or illustrate the service that is offered.
+                        </li>
+                        <li>
+                            Marketing campaigns must clearly identify who is providing the service (originator) and clearly state the expense of the service promoted.
+                        </li>
+                        <li>
+                            It is strictly forbidden to send messages to illegally collected numbers via MSG91 servers.
+                        </li>
+                    </ul>
                     <div className="my-3"></div>
                     Anti-Spam Policy Enforcement
                     <div className="my-3"></div>
@@ -151,24 +165,26 @@ const nospam = () => {
                     <div className="my-3"></div>
                     Your Right to Cancel / Money Back Guarantee
                     <div className="my-3"></div>
-                    <li>
-                    You can cancel account subscription in MSG91 Service ANYTIME and get the full refund (see refund policy). If your account is cancelled or suspended due to SPAM complaints NO refund will be allowable.
-                    </li>
-                    <li>
-                    Make sure all recipients are permission based (opt-in) i.e. agreed to receive sms from you.
-                    </li>
-                    <li>
-                    It is strictly forbidden to send SMS to the list of numbers addresses that was generated automatically by number extractor software; likewise you are not allowed to use purchased numbers for bulk use.
-                    </li>
-                    <li>
-                    Pay attention that each SMS that you send with the advertising, commercial, and informative purpose must contain your valid physical address.
-                    </li>
-                    <li>
-                    We reserve the right to stop providing service and block user account if even one of your email campaign gets over-the-top complaints.
-                    </li>
-                    <li>
-                    Accounts blocked due to exceeding complaints will be denied of any refund.
-                    </li>
+                    <ul>
+                        <li>
+                            You can cancel account subscription in MSG91 Service ANYTIME and get the full refund (see refund policy). If your account is cancelled or suspended due to SPAM complaints NO refund will be allowable.
+                        </li>
+                        <li>
+                            Make sure all recipients are permission based (opt-in) i.e. agreed to receive sms from you.
+                        </li>
+                        <li>
+                            It is strictly forbidden to send SMS to the list of numbers addresses that was generated automatically by number extractor software; likewise you are not allowed to use purchased numbers for bulk use.
+                        </li>
+                        <li>
+                            Pay attention that each SMS that you send with the advertising, commercial, and informative purpose must contain your valid physical address.
+                        </li>
+                        <li>
+                            We reserve the right to stop providing service and block user account if even one of your email campaign gets over-the-top complaints.
+                        </li>
+                        <li>
+                            Accounts blocked due to exceeding complaints will be denied of any refund.
+                        </li>
+                    </ul>
                     <div className="my-3"></div>
                     Our terms are aligned and subject to modification according to laws and guidelines by local government bodies like TRAI in India
                     <div className="my-3"></div>
