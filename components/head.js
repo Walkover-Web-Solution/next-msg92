b874382251e973db59b978113fe7543df5d8b013
piyusssshh <piyush@whozzat.com>
2024-04-24 15:28:08 +0530

    PIYUSSSSHH ||

--- components/head.js
+++ components/head.js
@@ -1,61 +1,61 @@
-import Head from 'next/head';
-import metaData from '@/data/metadata.json';
+import Head from "next/head";
+import metaData from "@/data/metadata.json";
 
 const Headcomp = (browserPath) => {
-    const countryList = ['in', 'ae', 'ph', 'sg', 'es', 'gb', 'us'];
+    const countryList = ["in", "ae", "ph", "sg", "es", "gb", "us"];
     const exptns = {
-        'gbl': [
-            'contact-us',
-            'blog',
-            'partners-and-integrations',
-            'about-us',
-            'developers',
-            'startups',
-            'become-a-partner',
-            'certificates',
-            'case-studies',
-            'terms-of-use',
-            'privacy-policy',
-            'gdpr',
-            'refund-policy',
-            'cookie-policy',
-            'startup-policy',
-            'fair-pricing-policy',
-            'no-spam',
-            'login',
-            'signup',
-            'signin',
-            'guide',
+        "gbl": [
+            "contact-us",
+            "blog",
+            "partners-and-integrations",
+            "about-us",
+            "developers",
+            "startups",
+            "become-a-partner",
+            "certificates",
+            "case-studies",
+            "terms-of-use",
+            "privacy-policy",
+            "gdpr",
+            "refund-policy",
+            "cookie-policy",
+            "startup-policy",
+            "fair-pricing-policy",
+            "no-spam",
+            "login",
+            "signup",
+            "signin",
+            "guide",
             "startups-form",
-            "thank-you"
+            "thank-you",
         ],
     };
 
-    const path = browserPath.browserPath.split('?')[0];
-    var meta = metaData[path] ? metaData[path] : { 'title': '', 'description': '' };
+    const path = browserPath.browserPath.split("?")[0];
+    var meta = metaData[path] ? metaData[path] : { "title": "", "description": "" };
     var pathPage, pathCountry, isOnlyGlobal;
-    const pathArrRaw = browserPath.browserPath.split('?');
-    const pathArr = pathArrRaw[0].split('/');
+    const pathArrRaw = browserPath.browserPath.split("?");
+    const pathArr = pathArrRaw[0].split("/");
     if (pathArr.length == 2) {
         if (countryList.includes(pathArr[1])) {
-            pathPage = '';
-            pathCountry = '/' + pathArr[1];
+            pathPage = "";
+            pathCountry = "/" + pathArr[1];
         } else {
             if (pathArr[1]) {
-                pathPage = '/' + pathArr[1];
+                pathPage = "/" + pathArr[1];
             } else {
-                pathPage = '';
+                pathPage = "";
             }
-            pathCountry = '';
+            pathCountry = "";
             isOnlyGlobal = exptns.gbl.includes(pathArr[1]);
         }
     } else {
         if (countryList.includes(pathArr[1])) {
-            pathCountry = '/' + pathArr[1];
-            pathPage = '/' + pathArr.slice(2).join('/');
+            pathCountry = "/" + pathArr[1];
+            pathPage = "/" + pathArr.slice(2).join("/");
         } else {
-            pathPage = '/' + pathArr.slice(1).join('/');
-            pathCountry = '';
+            pathPage = "/" + pathArr.slice(1).join("/");
+            pathCountry = "";
             isOnlyGlobal = exptns.gbl.includes(pathArr[1]);
         }
     }
@@ -70,7 +70,7 @@ const Headcomp = (browserPath) => {
                 />
                 <meta name="google-site-verification" content="RfcBy_Lv1Ao1j0eP8UlMjJ44ik5_1YDKsRQSNFr9jEQ" />
                 <link rel="icon" href="/fav.svg" />
-                {process.env.REDIRECT_URL === 'https://web.msg91.com' && (
+                {process.env.REDIRECT_URL === "https://web.msg91.com" && (
                     <>
                         <meta name="robots" content="noindex,nofollow" />
                         <meta name="googlebot" content="noindex,nofollow" />
@@ -78,19 +78,30 @@ const Headcomp = (browserPath) => {
                     </>
                 )}
                 <link rel="canonical" href={`https://msg91.com${pathArrRaw[0]}`} />
-                
-                
+
                 {!isOnlyGlobal && pathCountry && (
                     <>
-                        <link rel="alternate" hrefLang={`en-${pathArr[1].toUpperCase()}`} href={`https://msg91.com${pathCountry}${pathPage}`} />
+                        <link
+                            rel="alternate"
+                            hrefLang={`en-${pathArr[1].toUpperCase()}`}
+                            href={`https://msg91.com${pathCountry}${pathPage}`}
+                        />
                     </>
                 )}
-                {isOnlyGlobal || !pathCountry && (
-                    <>
-                        <link rel="alternate" hrefLang='en' href={`https://msg91.com${pathPage}`} />
-                    </>
-                )}
-             
+                {isOnlyGlobal ||
+                    (!pathCountry && (
+                        <>
+                            <link rel="alternate" hrefLang="en" href={`https://msg91.com${pathPage}`} />
+                        </>
+                    ))}
+                <script type="text/javascript">
+                    function googleTranslateElementInit(){" "}
+                    {new google.translate.TranslateElement({ pageLanguage: "en" }, "google_translate_element")}
+                </script>
+                <script
+                    type="text/javascript"
+                    src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+                ></script>
             </Head>
         </>
     );
--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,67 +1,79 @@
 import data from "@/data/content.json";
+import BRContent from "@/data/content/br.json";
 import IndexComp from "@/components/comps/indexComponent";
+import IndexDynamicComp from "@/components/comps/indexDynamicComponent";
 
 const mainpage = ({ pageData, params }) => {
-  var page = pageData?.pagename;
-  if (!pageData) {
-    return (
-      <div>
-        <h1>404 - Page Not Found</h1>
-        <p>The requested page was not found.</p>
-      </div>
-    );
-  } else if (page === "index") {
-    return (
-      <>
-        <IndexComp params={params} pageData={pageData} />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <h1>{pageData.head}</h1>
-      </>
-    );
-  }
+    console.log("🚀 ~ mainpage ~ params:", params);
+    var page = pageData?.pagename;
+    console.log("🚀 ~ mainpage ~ pageData:", pageData);
+    if (!pageData) {
+        return (
+            <div>
+                <h1>404 - Page Not Found</h1>
+                <p>The requested page was not found.</p>
+            </div>
+        );
+    } else if (page === "index") {
+        if (params?.country === "br") {
+            return (
+                <>
+                    <IndexDynamicComp params={params} pageData={BRContent.index} />
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <IndexComp params={params} pageData={pageData} />
+                </>
+            );
+        }
+    } else {
+        return (
+            <>
+                <h1>{pageData.head}</h1>
+            </>
+        );
+    }
 };
 
 export default mainpage;
 
 export async function getStaticPaths() {
-  const paths = [];
-  const countries = Object.keys(data);
-  countries.forEach((country) => {
-    const countryData = data[country] || {};
-    const pages = Object.keys(countryData);
-    pages.forEach((page) => {
-      paths.push({
-        params: {
-          pageslug: page, // Use 'pageslug' as the slug name
-          country: country,
-        },
-      });
+    const paths = [];
+    const countries = Object.keys(data);
+    countries.forEach((country) => {
+        const countryData = data[country] || {};
+        const pages = Object.keys(countryData);
+        pages.forEach((page) => {
+            paths.push({
+                params: {
+                    pageslug: page, // Use 'pageslug' as the slug name
+                    country: country,
+                },
+            });
+        });
     });
-  });
-  return {
-    paths,
-    fallback: false,
-  };
+    return {
+        paths,
+        fallback: false,
+    };
 }
 
 export async function getStaticProps({ params }) {
-  let { pageslug, country } = params; // Update variable name to 'pageslug'
-  if (country.length > 2 && pageslug == null) {
-    pageslug = country;
-    country = "global";
-  } else if (country.length == 2 && pageslug == null) {
-    pageslug = "index";
-  }
-  const countryData = data[country] || {};
-  const pageData = countryData[pageslug] || null; // Update variable name to 'pageslug'
-  return {
-    props: {
-      pageData,
-      params
-    },
-  };
+    let { pageslug, country } = params; // Update variable name to 'pageslug'
+    if (country.length > 2 && pageslug == null) {
+        pageslug = country;
+        country = "global";
+    } else if (country.length == 2 && pageslug == null) {
+        pageslug = "index";
+    }
+    const countryData = data[country] || {};
+    const pageData = countryData[pageslug] || null; // Update variable name to 'pageslug'
+    return {
+        props: {
+            pageData,
+            params,
+        },
+    };
 }
