fd411b4ec49114031264996211b42acfa94f94d3
piyusssshh <piyush@whozzat.com>
2024-07-10 18:16:11 +0530

    PIYUSSSSHH || LINT TEST

--- src/components/indexComp/indexComp.js
+++ src/components/indexComp/indexComp.js
@@ -2,7 +2,7 @@ import styles from './indexComp.module.scss';
 export default function IndexComp() {
     return (
         <>
-            <div className="text">Hello MSG91</div>
+            <div className="text ">Hello MSG91</div>
         </>
     );
 }
