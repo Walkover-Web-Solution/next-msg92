915a6665d16ed40dc1e0865af9acea62df46fdcc
piyusssshh <piyush@whozzat.com>
2024-07-10 18:12:07 +0530

    PIYUSSSSHH|| HUSKY TEST

--- src/components/indexComp/indexComp.js
+++ src/components/indexComp/indexComp.js
@@ -2,6 +2,9 @@ import styles from './indexComp.module.scss';
 export default function IndexComp() {
     return (
         <>
+            
+            
+            
             <div className="text">Hello MSG91</div>
         </>
     );
