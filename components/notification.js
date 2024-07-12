9d3be917f126dc135504d7b3a19da970c8cf18ba
piyusssshh <piyush@whozzat.com>
2024-07-12 16:44:27 +0530

    PIYUSSSSHH || country flag added

--- components/notification.js
+++ components/notification.js
@@ -8,45 +8,39 @@ import { getCookie, setCookie } from "@/components/utils";
 const baseUrl = process.env.LOGIN_URL;
 
 const Notification = ({ path, compData, pathArray }) => {
-    const [country, setCountry] = useState('');
+    const [country, setCountry] = useState("");
     const [language, setLanguage] = useState("English");
-    
+
     const baseUrl = process.env.API_BASE_URL;
 
     useEffect(() => {
         path = path ? path : getCookie("country_code");
-        
-        if (path?.startsWith('/')) {
+
+        if (path?.startsWith("/")) {
             path = path.substring(1);
         }
-        
-        
-        let lang = path === 'br' ? 'English' : 'Portuguese';
-        
+
+        let lang = path === "br" ? "English" : "Portuguese";
+
         setLanguage(lang);
-        
-        
-        if(!country){
-            setCountry('Global');
+
+        if (!country) {
+            setCountry("Global");
         }
-        
+
         for (let x in availableCountries) {
             if (path?.toUpperCase() === availableCountries[x].shortname) {
                 setCountry(availableCountries[x].name);
                 setCookie("country_code", availableCountries[x].shortname.toLowerCase(), 30);
                 break;
             }
-        }        
-
-
-        
+        }
     }, []);
     return (
         <>
             <div className="section b-bottom-1">
                 <div className="container d-flex align-items-center justify-content-between notification">
-                    <div className="my-2 d-flex align-items-center">
-                    </div>
+                    <div className="my-2 d-flex align-items-center"></div>
                     <div className="d-flex gap-4 align-items-center justify-content-end">
                         {(pathArray[1] === "br" || pathArray[1] === "br-pt") && (
                             <div className="dropdown d-flex align-items-center">
@@ -58,12 +52,10 @@ const Notification = ({ path, compData, pathArray }) => {
                                     role="button"
                                 >
                                     <MdGTranslate className="me-1" />
-                                    <span className="c-fs-5 d-flex align-items-center">
-                                        {language}
-                                    </span>
+                                    <span className="c-fs-5 d-flex align-items-center">{language}</span>
                                     <MdExpandMore className="ms-1" />
                                 </a>
-                                <ul className="dropdown-menu" >
+                                <ul className="dropdown-menu">
                                     <li>
                                         <a href="/br" className="dropdown-item c-fs-5">
                                             English
@@ -86,53 +78,67 @@ const Notification = ({ path, compData, pathArray }) => {
                                 aria-expanded="false"
                                 role="button"
                             >
-                                <MdLanguage className="me-1" />
+                                {path === "/?" ? (
+                                    <MdLanguage className="me-1" />
+                                ) : (
+                                    <img src={`/assets/country-flags${path === '/br-pt' ? '/br' : path }.svg`} className="nav-flag" />
+                                )}
+
                                 <span className="c-fs-5 d-flex align-items-center">{country}</span>
                                 <MdExpandMore className="ms-1" />
                             </a>
                             <ul className="dropdown-menu" id="change-country">
                                 <li>
-                                    <a href="/?" className="dropdown-item c-fs-5">
+                                    <a href="/?" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                        <MdLanguage className="me-2" />
                                         Global
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/in" className="dropdown-item c-fs-5">
+                                    <a href="/in" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                        <img src="/assets/country-flags/in.svg" className="nav-flag" />
                                         India
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/br-pt" className="dropdown-item c-fs-5">
+                                    <a href="/br-pt" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                        <img src="/assets/country-flags/br.svg" className="nav-flag" />
                                         Brazil
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/ae" className="dropdown-item c-fs-5">
+                                    <a href="/ae" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                        <img src="/assets/country-flags/ae.svg" className="nav-flag" />
                                         United Arab Emirates
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/ph" className="dropdown-item c-fs-5">
+                                    <a href="/ph" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                        <img src="/assets/country-flags/ph.svg" className="nav-flag" />
                                         Philippines
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/sg" className="dropdown-item c-fs-5">
+                                    <a href="/sg" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                        <img src="/assets/country-flags/sg.svg" className="nav-flag" />
                                         Singapore
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/es" className="dropdown-item c-fs-5">
+                                    <a href="/es" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                        <img src="/assets/country-flags/es.svg" className="nav-flag" />
                                         Spain
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/gb" className="dropdown-item c-fs-5">
+                                    <a href="/gb" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                        <img src="/assets/country-flags/gb.svg" className="nav-flag" />
                                         United Kingdom
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/us" className="dropdown-item c-fs-5">
+                                    <a href="/us" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                        <img src="/assets/country-flags/us.svg" className="nav-flag" />
                                         United States
                                     </a>
                                 </li>
