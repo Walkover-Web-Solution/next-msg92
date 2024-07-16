24d28fc2a97bb9e9b67d215431b92012072aa539
piyusssshh <piyush@whozzat.com>
2024-07-16 17:52:15 +0530

    FLAG ISSUE FIXED

--- components/notification.js
+++ components/notification.js
@@ -1,20 +1,52 @@
 import { MdLanguage, MdCall, MdExpandMore, MdLogin, MdGTranslate } from "react-icons/md";
 import React, { useEffect, useState } from "react";
-import $ from "jquery";
-import countries from "@/data/countries.json";
 import availableCountries from "@/data/available-countries.json";
 import { getCookie, setCookie } from "@/components/utils";
+import { useRouter } from "next/router";
 
 const baseUrl = process.env.LOGIN_URL;
-
 
 const Notification = ({ path, compData, pathArray }) => {
     const [country, setCountry] = useState("");
     const [countryShort, setCountryShort] = useState("/?");
-    const [language, setLanguage] = useState("English");    
+    const [language, setLanguage] = useState("English");
+
+    const countryArray = ["in", "us", "gb", "ph", "br-pt", "br", "sg", "ae", "es"];
+    const products = [
+        "/sms",
+        "/email",
+        "/voice",
+        "/whatsapp",
+        "/rcs",
+        "/otp",
+        "/hello",
+        "/numbers",
+        "/segmento",
+        "/campaign",
+        "/free-whatsapp-link-generator",
+        "/pricing",
+        "/pricing/sms",
+        "/shorturl",
+    ];
+    const router = useRouter();
+    const rawPath = router.asPath;
+    const rawPathArray = rawPath.split("/").slice(1);
+    console.log("🚀 ~ Notification ~ rawPathArray:", rawPathArray);
+    var basePath;
+    if (
+        countryArray.some((countryCode) => rawPath.startsWith(`/${countryCode}`)) ||
+        products.some((product) => rawPath.split("/")[1] === product.substring(1))
+    ) {
+        console.log("rawPath starts with a country code from countryArray");
+        basePath = rawPath.replace(/^\/(in|us|gb|ph|br-pt|br|sg|ae|es)/, "");
+        console.log("🚀 ~ Notification ~ rawPath:", basePath);
+    } else {
+        basePath = rawPath;
+        console.log("🚀 ~ Notification ~ basePath:", basePath);
+    }
 
     useEffect(() => {
-        path = path ? path : getCookie("country_code");        
+        path = path ? path : getCookie("country_code");
         if (path?.startsWith("/")) {
             path = path.substring(1);
         }
@@ -25,14 +57,14 @@ const Notification = ({ path, compData, pathArray }) => {
 
         if (!country) {
             setCountry("Global");
-            setCountryShort("/?")
+            setCountryShort("/?");
         }
 
         for (let x in availableCountries) {
             if (path?.toUpperCase() === availableCountries[x].shortname) {
                 setCountry(availableCountries[x].name);
                 setCookie("country_code", availableCountries[x].shortname.toLowerCase(), 30);
-                setCountryShort(availableCountries[x].shortname.toLowerCase())
+                setCountryShort(availableCountries[x].shortname.toLowerCase());
                 break;
             }
         }
@@ -79,10 +111,18 @@ const Notification = ({ path, compData, pathArray }) => {
                                 aria-expanded="false"
                                 role="button"
                             >
-                                {countryShort === '/?' || countryShort === '?' || countryShort === '' || countryShort === '/' ? (
+                                {countryShort === "/?" ||
+                                countryShort === "?" ||
+                                countryShort === "" ||
+                                countryShort === "/" ? (
                                     <MdLanguage className="me-1" />
                                 ) : (
-                                    <img src={`/assets/country-flags/${countryShort === '/br-pt' ? '/br' : countryShort }.svg`} className="nav-flag" />
+                                    <img
+                                        src={`/assets/country-flags/${
+                                            countryShort === "br-pt" ? "br" : countryShort
+                                        }.svg`}
+                                        className="nav-flag"
+                                    />
                                 )}
 
                                 <span className="c-fs-5 d-flex align-items-center">{country}</span>
@@ -90,55 +130,55 @@ const Notification = ({ path, compData, pathArray }) => {
                             </a>
                             <ul className="dropdown-menu" id="change-country">
                                 <li>
-                                    <a href="/?" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                    <a href={"/?"} className="dropdown-item c-fs-5 d-flex align-items-center">
                                         <MdLanguage className="me-2" />
                                         Global
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/in" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                    <a href={basePath} className="dropdown-item c-fs-5 d-flex align-items-center">
                                         <img src="/assets/country-flags/in.svg" className="nav-flag" />
                                         India
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/br-pt" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                    <a href={basePath} className="dropdown-item c-fs-5 d-flex align-items-center">
                                         <img src="/assets/country-flags/br.svg" className="nav-flag" />
                                         Brazil
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/ae" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                    <a href={basePath} className="dropdown-item c-fs-5 d-flex align-items-center">
                                         <img src="/assets/country-flags/ae.svg" className="nav-flag" />
                                         United Arab Emirates
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/ph" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                    <a href={basePath} className="dropdown-item c-fs-5 d-flex align-items-center">
                                         <img src="/assets/country-flags/ph.svg" className="nav-flag" />
                                         Philippines
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/sg" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                    <a href={basePath} className="dropdown-item c-fs-5 d-flex align-items-center">
                                         <img src="/assets/country-flags/sg.svg" className="nav-flag" />
                                         Singapore
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/es" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                    <a href={basePath} className="dropdown-item c-fs-5 d-flex align-items-center">
                                         <img src="/assets/country-flags/es.svg" className="nav-flag" />
                                         Spain
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/gb" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                    <a href={basePath} className="dropdown-item c-fs-5 d-flex align-items-center">
                                         <img src="/assets/country-flags/gb.svg" className="nav-flag" />
                                         United Kingdom
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="/us" className="dropdown-item c-fs-5 d-flex align-items-center">
+                                    <a href={basePath} className="dropdown-item c-fs-5 d-flex align-items-center">
                                         <img src="/assets/country-flags/us.svg" className="nav-flag" />
                                         United States
                                     </a>
