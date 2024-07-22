9a5817f90a95a8625a0424ea588e8e7b67c0bdab
Ashish Yadav <ashish@walkover>
2024-07-22 23:11:25 +0530

    resolve conflicst

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import IndexComp from '@/components/indexComp/indexComp';
-import ProductComp from '@/components/productComp/productComp';
-import NotificationBarComp from '@/components/notificationBarComp/notificationBarComp';
-
-const Components = {
-    IndexComp,
-    ProductComp,
-    NotificationBarComp
-};
-
-export default function Page(props) {
-    if (props.components?.length > 0) {
-        return (
-            <div>
-                {props.components.map((comp) => {
-                    var Component = Components[comp.comp];
-                    if (!Component) {
-                        console.error(`Component ${comp.comp} not found`);
-                        return null;
-                    }
-                    return <Component />;
-                })}
-            </div>
-        );
-    }
-=======
 /* components */
 import HeadComp from '../components/headComp';
 import NotificationBarComp from '../components/notificationBarComp/notificationBarComp';
@@ -52,7 +25,6 @@ export default function Page(props) {
       })}
     </div>
   );
->>>>>>> c48e7df (fixed comp call by string)
 }
 
 export const getStaticPaths = async () => {
@@ -63,20 +35,6 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async (context) => {
-<<<<<<< HEAD
-    const params = context.params.slug;
-    const res = await fetch('http://localhost:3000/api/fetch?lang=en&country=in');
-    const data = await res.json();
-    if (!res.ok) {
-        return {
-            notFound: true,
-        };
-    }
-
-    return {
-        props: data,
-    };
-=======
   const params = context.params  
   const res = await fetch('http://localhost:3000/api/data', {
     method: 'POST',
@@ -91,5 +49,4 @@ export const getStaticProps = async (context) => {
   return {
     props: data,
   };  
->>>>>>> c48e7df (fixed comp call by string)
 };
