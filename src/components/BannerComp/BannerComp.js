9914b49d26aceef9e33521a1d2b3bc375abca54e
piyusssshh <piyush@whozzat.com>
2024-08-29 17:16:05 +0530

    PIYUSSSSHH || product page icon added

--- src/components/BannerComp/BannerComp.js
+++ src/components/BannerComp/BannerComp.js
@@ -8,6 +8,12 @@ export default function BannerComp({ data }) {
             <div className='container my-20 flex gap-6 relative lg:h-[560px] flex-col lg:flex-row '>
                 <div className='flex flex-col lg:gap-12 gap-8  h-full justify-center lg:w-1/2 z-10'>
                     <div className='flex flex-col gap-6'>
+                        {data?.product && (
+                            <div className='flex items-center gap-2'>
+                                <Image src={data?.product?.icon} width={40} height={40} alt={data?.product?.name} />
+                                <span className='font-semibold text-2xl'>{data?.product?.name}</span>
+                            </div>
+                        )}
                         <span className={styles.tagline}>{data?.tagline}</span>
                         <h1 className='heading'>{data?.heading}</h1>
                         <h2 className='suheading'>{data?.subheading}</h2>
@@ -16,14 +22,15 @@ export default function BannerComp({ data }) {
                     <LinkButton href={'/'} content={data?.getstarted_btn} customClasses={'btn btn-primary btn-md '} />
                     <TrustedByComp data={data?.trustedByComp} />
                 </div>
-
-                <Image
-                    src={data?.banner_img}
-                    width={1080}
-                    height={1080}
-                    className='lg:h-full w-auto lg:absolute right-2 top-0 bottom-0'
-                    alt={data?.tagline}
-                />
+                <div className='lg:absolute right-2 top-0 bottom-0 flex'>
+                    <Image
+                        src={data?.banner_img}
+                        width={1080}
+                        height={1080}
+                        className='h-auto w-full max-w-[600px] m-auto'
+                        alt={data?.tagline}
+                    />
+                </div>
             </div>
         </>
     );
