9c6f2f5bbb1cd3ff8753f1fe3a3544ea398548d2
piyusssshh <piyush@whozzat.com>
2024-09-24 17:52:22 +0530

    PIYUSSSSHH || RCS pricing done

--- src/components/PricingComp/PricingRCS/PricingRCS.js
+++ src/components/PricingComp/PricingRCS/PricingRCS.js
@@ -1,3 +1,10 @@
 export default function PricingRCS() {
-    return <>PricingRCS</>;
+    return (
+        <>
+            <div className='p-8 flex flex-col gap-4 bg-white h-fit rounded'>
+                <h1 className='text-2xl'>Connect to our team for the custimized pricing</h1>
+                <button className='btn btn-primary btn-md'>Get Started</button>
+            </div>
+        </>
+    );
 }
