5ee5a05e339bdcaa49972da6bb59cb1e47188f31
piyusssshh <piyush@whozzat.com>
2024-10-11 19:40:59 +0530

    PIYUSSSSHH || about us page

--- src/components/AboutUsComp/AboutUsComp.js
+++ src/components/AboutUsComp/AboutUsComp.js
@@ -1,105 +1,105 @@
 import Image from 'next/image';
+import { MdAutoFixHigh } from 'react-icons/md';
 export default function AboutUsComp({ pageInfo, data }) {
     return (
         <>
-            <div className='container gap-6 py-20'>
-                <h1 class='text-4xl font-bold mb-2 weight-700'>{data?.heading || ' this is heading '}</h1>
-                <h2 class='text-xl font-normal mb-4 w-1/2 gap-6'>{data?.subheading || ' this is heading '}</h2>
-
-                <div className=' '>
-                    {' '}
-                    <Image
-                        src={data?.banner_img}
-                        alt='walkoverteam image'
-                        width={1500}
-                        height={495}
-                        className=' w-full h-auto'
-                    />
-                </div>
+            <div className='container flex flex-col cont_gap cont_p '>
+                <h1 class='text-5xl font-bold '>{data?.heading}</h1>
+                <h2 class='text-xl md:w-1/2 gap-6'>{data?.subheading}</h2>
+                <Image
+                    src={data?.banner_img}
+                    alt='walkoverteam'
+                    width={1500}
+                    height={495}
+                    className=' w-full h-auto rounded-md'
+                />
             </div>
-            <div className='container py-20 gap-6'>
-                <h2 className='text-3xl font-bold mb-4'>{data?.offer?.heading}</h2>
-                <div className='grid lg:grid-cols-2 sm:grid-cols-1 grid-cols-1 gap-6'>
-                    {data?.offer?.content?.length > 0 &&
-                        data?.offer?.content.map((item, index) => {
-                            return (
-                                <div key={index} className='bg-neutral p-4 rounded-lg flex items-center'>
-                                    <div className='flex-shrink-0 m-4'>
-                                        <Image src={item?.icon} alt={item?.description} width={40} height={40} />
-                                    </div>
-                                    <div className=''>
+            <div className='bg-gray'>
+                <div className='container flex flex-col cont_gap cont_p'>
+                    <h2 className='text-3xl font-bold'>{data?.offer?.heading}</h2>
+                    <div className='grid lg:grid-cols-2 sm:grid-cols-1 grid-cols-1 md:gap-10 gap-4'>
+                        {data?.offer?.content?.length > 0 &&
+                            data?.offer?.content.map((item, index) => {
+                                return (
+                                    <div key={index} className='bg-white p-4 rounded-lg flex gap-3 items-center'>
+                                        <MdAutoFixHigh fontSize={30} />
                                         <p className='text-lg'>{item?.description}</p>
                                     </div>
-                                </div>
-                            );
-                        })}
+                                );
+                            })}
+                    </div>
                 </div>
             </div>
-            <div className='container py-20 gap-6'>
-                <h2 className='text-3xl font-bold mb-4'>{data?.team?.heading}</h2>
-                <p className='text-lg mb-8'>{data?.team?.description}</p>
-                <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6'>
+
+            <div className='container flex flex-col cont_gap cont_p'>
+                <h2 className='text-3xl font-bold'>{data?.team?.heading}</h2>
+                <p className='text-lg'>{data?.team?.description}</p>
+                <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-10 gap-6'>
                     {data?.team?.content?.length > 0 &&
                         data?.team?.content.map((item, index) => {
                             return (
-                                <div key={index} className='rounded-lg flex flex-col items-start bg-neutral p-8'>
-                                    <div className='flex-shrink-0 '>
-                                        <Image src={item?.icon} alt={item?.name} width={400} height={400} />
-                                    </div>
-                                    <h3 className='text-lg font-bold mb-2 '>{item?.name}</h3>
-                                    <p className='text-md font-normal mb-4'>{item?.post}</p>
-                                    <p className='text-md'>{item?.description}</p>
-                                </div>
-                            );
-                        })}
-                </div>
-            </div>
-            <div className='bg-neutral py-20 '>
-                <div className=' container gap-6 text-center'>
-                    <p>{data?.hiring?.content}</p>
-                    <a href={data?.hiring?.link} className='text-blue-500'>
-                        {data?.hiring?.linkText}
-                    </a>
-                </div>
-            </div>
-            <div className='container py-20 gap-6'>
-                <h2 className='text-3xl font-bold '>{data?.teamStrength?.heading}</h2>
-                <div className=' py-4'>
-                    {' '}
-                    <Image
-                        src={data?.teamStrength?.img}
-                        alt='walkoverteam image'
-                        width={1500}
-                        height={495}
-                        className=' w-full h-auto'
-                    />
-                </div>
-                <h2 class='text-xl font-normal'>{data?.teamStrength?.description || ' this is heading '}</h2>
-            </div>
-            <div className='container bg-neutral py-20 gap-6'>
-                <h2 className='text-3xl font-bold mb-8'>{data?.clientSays?.heading}</h2>
-                <div className='  grid lg:grid-cols-3 sm:grid-cols-1 grid-cols-1 gap-6'>
-                    {data?.clientSays?.card?.length > 0 &&
-                        data?.clientSays?.card.map((item, index) => {
-                            return (
-                                <div key={index} className='bg-white rounded-lg  flex flex-col'>
-                                    <div className='flex-grow m-4'>
-                                        <p className='text-lg'>{item?.description}</p>
+                                <div key={index} className='flex flex-col gap-6 rounded p-6 bg-neutral '>
+                                    <Image
+                                        className='rounded'
+                                        src={item?.icon}
+                                        alt={item?.name}
+                                        width={400}
+                                        height={400}
+                                    />
+                                    <div>
+                                        <h3 className='text-2xl font-bold'>{item?.name}</h3>
+                                        <p className='text-lg'>{item?.post}</p>
                                     </div>
 
-                                    <div className='flex p-4'>
-                                        <div className='w-1/4'>
-                                            <div class='skeleton h-16 w-16 color-gray-200'></div>
-                                            {/* <Image src={item?.icon} alt={item?.name} width={100} height={100} /> */}
-                                        </div>
-                                        <div className='flex flex-col w-3/4'>
-                                            <div className='text-lg font-bold'>{item?.name}</div>
-                                            <div className='font-normal'>{item?.post}</div>
-                                        </div>
-                                    </div>
+                                    <p className='text-lg'>{item?.description}</p>
                                 </div>
                             );
                         })}
+                </div>
+            </div>
+
+            <div className='bg-neutral'>
+                <div className=' flex flex-col items-center cont_gap cont_p text-center text-lg'>
+                    <p className='md:w-2/4'>
+                        {data?.hiring?.content}{' '}
+                        <a href={data?.hiring?.link} className='text-link active-link'>
+                            {data?.hiring?.linkText}
+                        </a>
+                    </p>
+                </div>
+            </div>
+
+            <div className='container flex flex-col cont_gap cont_p'>
+                <h2 className='text-3xl font-bold'>{data?.teamStrength?.heading}</h2>
+                <Image
+                    src={data?.teamStrength?.img}
+                    alt='walkoverteam image'
+                    width={1500}
+                    height={495}
+                    className=' w-full h-auto'
+                />
+                <p class='text-xl font-normal'>{data?.teamStrength?.description}</p>
+            </div>
+            <div className='bg-neutral'>
+                <div className='container flex flex-col cont_gap cont_p'>
+                    <h2 className='text-3xl font-bold'>{data?.clientSays?.heading}</h2>
+                    <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-10 gap-4 '>
+                        {data?.clientSays?.card?.length > 0 &&
+                            data?.clientSays?.card.map((item, index) => {
+                                return (
+                                    <div key={index} className='bg-white rounded  flex flex-col md:p-6 p-4 gap-6'>
+                                        <p className='text-lg'>{item?.description}</p>
+                                        <div className='flex gap-4 mt-auto'>
+                                            <div class='skeleton h-[40px] w-[40px] color-gray-200'></div>
+                                            <div className='flex flex-col'>
+                                                <h3 className='text-lg font-bold'>{item?.name}</h3>
+                                                <p className='font-normal'>{item?.post}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                    </div>
                 </div>
             </div>
         </>
