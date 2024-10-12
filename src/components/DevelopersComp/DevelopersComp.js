e5e8cda5314a2e6670249c2dedcbcfc81792e817
piyusssshh <piyush@whozzat.com>
2024-10-12 11:57:38 +0530

    PIYUSSSSHH || developers page

--- src/components/DevelopersComp/DevelopersComp.js
+++ src/components/DevelopersComp/DevelopersComp.js
@@ -1,96 +1,77 @@
+import Image from 'next/image';
+import { MdBeenhere, MdOutlineBeenhere } from 'react-icons/md';
+
 export default function DevelopersComp({ pageInfo, data }) {
     return (
         <>
-            <div className='container gap-6 py-20'>
-                <h1 class='text-4xl font-bold py-2 weight-700'>{data?.heading || ' this is heading '}</h1>
-                <h2 class='text-xl font-normal py-4  w-full lg:w-1/2 gap-6'>
-                    {data?.subheading || ' this is heading '}
-                </h2>
-                <a href={data?.doc_btn_link} target='_blank' className='btn btn-wide bg-black py-2 text-white'>
-                    {data?.doc_btn}
+            <div className='container flex flex-col cont_gap cont_p'>
+                <h1 className='text-5xl font-bold'>{data?.heading}</h1>
+                <h2 className='text-xl md:w-1/2 gap-6'>{data?.subheading}</h2>
+                <a href={data?.doc_btn_link} target='_blank'>
+                    <button className='btn btn-primary'>{data?.doc_btn}</button>
                 </a>
             </div>
             <div className='bg-neutral'>
-                <div className='container gap-6 py-20'>
-                    <div className=' row'>
-                        <h1 class='text-4xl font-bold py-2 weight-700'>
-                            {data?.integrate?.heading || ' this is heading '}
-                        </h1>
-                    </div>
+                <div className='container flex flex-col cont_gap cont_p'>
+                    <h2 className='text-4xl font-bold'>{data?.integrate?.heading}</h2>
 
-                    <div className='row'>
-                        <p class='text-lg font-normal py-4 gap-6'>{data?.integrate?.content || ' this is content '}</p>
-                    </div>
-                    <div className='row'>
-                        <h2 class='text-xl font-normal py-4 w-1/2 gap-6'>
-                            {data?.integrate?.subheading || ' this is heading '}
-                        </h2>
-                    </div>
+                    <p className='text-lg'>{data?.integrate?.content}</p>
+                    <p className='text-xl'>{data?.integrate?.subheading}</p>
                 </div>
             </div>
-            <div className='container gap-6 py-20'>
-                <h1 class='text-4xl font-bold py-2 weight-700'>
-                    {data?.communication?.heading || ' this is heading '}
-                </h1>
+            <div className='container flex flex-col cont_gap cont_p'>
+                <h2 className='text-4xl font-bold'>{data?.communication?.heading}</h2>
 
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                     {data?.communication?.card.map((card, index) => (
-                        <div key={index} className='bg-neutral p-4 rounded-lg flex items-center'>
-                            <div className='flex gap-4'>
-                                <div className='w-1/4 color-black'>
-                                    <img src={card.icon} alt={card.tag} className='w-full h-auto' />
-                                </div>
-                                <div className=''>
-                                    <p className='text-lg text-black'>{card.tag}</p>
-                                </div>
-                            </div>
+                        <div key={index} className='bg-neutral p-4 rounded-lg flex items-center gap-4'>
+                            <MdOutlineBeenhere fontSize={40} className='text-accent' />
+                            <p className='text-lg'>{card?.tag}</p>
                         </div>
                     ))}
                 </div>
             </div>
-            <div className='container gap-6 py-20'>
-                <h1 className='text-4xl font-bold py-2 weight-500'>{data?.msg91api?.heading || ' this is heading '}</h1>
-                {data?.msg91api?.comp.map((comp, index) => (
-                    <div key={index}>
-                        <h2 className='text-2xl font-bold py-2 weight-500'>{comp.heading || ' this is heading '}</h2>
-                        <p className='text-lg font-normal py-2 '>{comp.content || ' this is heading '}</p>
-                    </div>
-                ))}
-            </div>
-
-            <div className='container mx-auto py-20 px-4 sm:px-6 lg:px-8 gap-6'>
-                <div className='grid gap-4'>
-                    <div className='p-4 rounded-lg flex flex-col md:flex-row items-center'>
-                        <div className='ml-0 md:ml-4 mb-4 md:mb-0 md:w-1/2'>
-                            <h2 className='text-xl font-bold mb-2'>{data?.customcode?.heading || 'Default Heading'}</h2>
-                            <p className='text-lg'>{data?.customcode?.content || 'Default content goes here.'}</p>
+            <div className='bg-neutral'>
+                <div className='container flex flex-col cont_gap cont_p'>
+                    <h2 className='text-4xl font-bold'>{data?.msg91api?.heading}</h2>
+                    {data?.msg91api?.comp.map((comp, index) => (
+                        <div key={index} className='flex flex-col gap-2'>
+                            <h3 className='text-2xl font-bold'>{comp?.heading}</h3>
+                            <p className='text-lg'>{comp?.content}</p>
                         </div>
-
-                        <div className='flex-shrink-0 w-full md:w-1/2'>
-                            <img
-                                src={data?.customcode?.code || 'default-image-url'}
-                                alt='code'
-                                className='w-full h-auto object-contain'
-                            />
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
 
-            <div className='container mx-auto py-20 px-4 sm:px-6 lg:px-8 gap-6'>
-                <h2 className='text-2xl font-bold py-2'>{data?.setup?.heading || 'This is heading'}</h2>
-                <p className='text-lg mb-8'>{data?.setup?.description}</p>
+            <div className='container flex flex-col md:flex-row items-center cont_gap cont_p'>
+                <div className='flex flex-col gap-4'>
+                    <h2 className='text-4xl font-bold'>{data?.customcode?.heading}</h2>
+                    <p className='text-lg'>{data?.customcode?.content}</p>
+                </div>
+                <Image src={data?.customcode?.code} alt='code' width={700} height={700} className='w-1/2' />
+            </div>
 
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6'>
-                    {data?.setup?.type.map((type, index) => (
-                        <div key={index} className='bg-neutral p-4 rounded-lg flex items-center justify-center'>
-                            <div className='w-full'>
-                                <a href={type.url} target='_blank' rel='noopener noreferrer'>
-                                    <img src={type.icon} alt={type.name} className='w-full h-auto object-contain' />
-                                </a>
+            <div className='bg-neutral'>
+                <div className='container flex flex-col cont_gap cont_p'>
+                    <div className='flex flex-col gap-2'>
+                        <h2 className='text-2xl font-bold py-2'>{data?.setup?.heading}</h2>
+                        <p className='text-lg mb-8'>{data?.setup?.description}</p>
+                    </div>
+
+                    <div className=' flex gap-4 sm:flex-row flex-col'>
+                        {data?.setup?.type.map((app, index) => (
+                            <div
+                                key={index}
+                                className='bg-primary p-4 rounded flex  items-center sm:w-[260px] w-full gap-2'
+                            >
+                                <Image width={40} height={40} src={app.icon} alt={app.name} />
+                                <div className='flex flex-col '>
+                                    <p className='text-sm text-white uppercase'>{app?.text}</p>
+                                    <p className=' text-xl text-white'>{app?.name}</p>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
