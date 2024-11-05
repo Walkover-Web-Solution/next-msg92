4be45d0c07d02d9bdbb9e024fe984bdd5bb69873
vanshika <guptavanshika9630@gmail.com>
2024-11-05 17:46:47 +0530

    blog design change

--- src/components/Guide/page/PostList.js
+++ src/components/Guide/page/PostList.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import Pagination from './Pagination';
 import PostItem from './PostItem';
+import MenuBarComp from '@/components/menuBarComp/menuBarComp';
 
 export default function PostList({ posts, tags, pagination }) {
     return (
-        <div className='bg-[#cce1dc]'>
-            <div className='container  mx-auto gap-6 py-20 '>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 '>
-                    {posts?.map((post, index) => post.slug && <PostItem key={index} post={post} />)}
-                </div>
-                <div>
-                    <Pagination
-                        current={pagination.current}
-                        pages={pagination.pages}
-                        link={{
-                            href: (page) => (page === 1 ? '/guide' : '/guide/page/[page]'),
-                            as: (page) => (page === 1 ? null : '/guide/page/' + page),
-                        }}
-                    />
+        <>
+            <div className='bg-[#cce1dc]'>
+                <div className='container  mx-auto gap-6 py-20 '>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 '>
+                        {posts?.map((post, index) => post.slug && <PostItem key={index} post={post} />)}
+                    </div>
+                    <div>
+                        <Pagination
+                            current={pagination.current}
+                            pages={pagination.pages}
+                            link={{
+                                href: (page) => (page === 1 ? '/guide' : '/guide/page/[page]'),
+                                as: (page) => (page === 1 ? null : '/guide/page/' + page),
+                            }}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
