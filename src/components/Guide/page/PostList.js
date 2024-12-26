b9ec4475049d55ddb3239f6d65ecce933923b259
piyusssshh <piyush@whozzat.com>
2024-12-26 13:32:19 +0530

    53 | h1 added in index page and blogs

--- src/components/Guide/page/PostList.js
+++ src/components/Guide/page/PostList.js
@@ -7,7 +7,10 @@ export default function PostList({ posts, tags, pagination }) {
     return (
         <>
             <div className='bg-[#cce1dc]'>
-                <div className='container  mx-auto gap-6 py-20 '>
+                <div className='container flex flex-col  mx-auto gap-6 py-20 '>
+                    <h1 className='text-3xl font-semibold text-center'>
+                        Explore the Latest Insights and Updates on MSG91
+                    </h1>
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 '>
                         {posts?.map((post, index) => post.slug && <PostItem key={index} post={post} />)}
                     </div>
