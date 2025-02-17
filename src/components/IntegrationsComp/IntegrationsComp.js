63ea122e9be091507c278b9c139140998f24d034
piyusssshh <piyush@whozzat.com>
2025-02-17 11:58:29 +0530

    WEB91-64 | integrations page design cange

--- src/components/IntegrationsComp/IntegrationsComp.js
+++ src/components/IntegrationsComp/IntegrationsComp.js
@@ -20,6 +20,41 @@ export default function IntegrationsComp({ pageInfo, data }) {
             </div>
             <div className='bg-neutral'>
                 <div className='container flex flex-col gap-6 py-20'>
+                    <h2 class='text-3xl font-bold '>{data?.plugins_by_msg91?.heading || 'Plugins by MSG91'}</h2>
+                    {data?.plugins_by_msg91?.subheading && (
+                        <p class='text-xl font-normal'>{data?.plugins_by_msg91?.subheading}</p>
+                    )}
+
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-10'>
+                        {data?.plugins_by_msg91?.plugins.map((card, index) => (
+                            <div className='flex flex-col  p-6 gap-2 bg-white rounded shadow h-full'>
+                                <div className='flex flex-col gap-4'>
+                                    <img src={card.icon} alt={card.name} className=' h-8 w-fit' />
+                                    <h3 className='text-lg font-bold mb-2'>{card.name}</h3>
+                                </div>
+                                <p className='mb-4'>{card.description}</p>
+                                <div className='flex items-center gap-2 mt-auto'>
+                                    {card?.channels?.length > 0 &&
+                                        card.channels.map((channel, index) => {
+                                            return (
+                                                <a
+                                                    target='_blank'
+                                                    className='text-sm text-link active-link'
+                                                    href={channel?.link}
+                                                    key={index}
+                                                >
+                                                    {channel?.channel}
+                                                </a>
+                                            );
+                                        })}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className='bg-neutral'>
+                <div className='container flex flex-col gap-6 py-20'>
                     <h2 class='text-3xl font-bold '>{data?.addon?.heading || ' this is heading '}</h2>
                     <p class='text-xl font-normal'>{data?.addon?.subheading || ' this is heading '}</p>
 
