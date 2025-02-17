d0bf4fcfe1181d5d851eb5f711412b907e9c6e44
piyusssshh <piyush@whozzat.com>
2025-02-17 14:45:45 +0530

    WEB91-63 | whatsapp us contnet update

--- src/components/FeatureComp/FeatureComp.js
+++ src/components/FeatureComp/FeatureComp.js
@@ -22,7 +22,7 @@ export default function FeatureComp({ data }) {
                                             : 'flex flex-col lg:col-span-1 col-span-2'
                                     }`}
                                 >
-                                    <div className='lg:p-12 p-4 flex flex-col gap-2'>
+                                    <div className='lg:p-12 p-4 flex flex-col gap-3'>
                                         {feature?.icon && <Icon className='text-5xl text-[#F7DC6F]' />}
 
                                         <h3 className='text-2xl font-semibold'>{feature?.name}</h3>
@@ -30,24 +30,69 @@ export default function FeatureComp({ data }) {
                                             <span className='font-bold'>{feature?.subheading} </span>
                                             <span dangerouslySetInnerHTML={{ __html: feature?.content }}></span>
                                         </span>
-                                        {}
-                                        <div
-                                            className={`text-lg ${openedFeatures.includes(index) ? 'block' : 'hidden'}`}
-                                            dangerouslySetInnerHTML={{ __html: feature?.more_content }}
-                                        ></div>
-                                        <div
-                                            onClick={() => {
-                                                setOpenedFeatures((prevOpenedFeatures) => {
-                                                    if (prevOpenedFeatures.includes(index)) {
-                                                        return prevOpenedFeatures.filter((i) => i !== index);
-                                                    } else {
-                                                        return [...prevOpenedFeatures, index];
-                                                    }
-                                                });
-                                            }}
-                                        >
-                                            <MoreButton open={openedFeatures.includes(index)} content={'Read More'} />
-                                        </div>
+                                        {feature?.more_content && (
+                                            <>
+                                                {feature?.more_content.content && (
+                                                    <div
+                                                        className={`text-lg ${
+                                                            openedFeatures.includes(index) ? 'block' : 'hidden'
+                                                        }`}
+                                                        dangerouslySetInnerHTML={{
+                                                            __html: feature?.more_content.content,
+                                                        }}
+                                                    ></div>
+                                                )}
+                                                {feature?.more_content?.usecases?.length > 0 && (
+                                                    <div
+                                                        className={` ${
+                                                            openedFeatures.includes(index) ? 'block' : 'hidden'
+                                                        }`}
+                                                    >
+                                                        <h4 className='font-semibold text-lg'>Use Cases:</h4>
+                                                        <ul className='flex flex-col gap-1 list-disc'>
+                                                            {feature?.more_content?.usecases?.map((usecase, index) => {
+                                                                return (
+                                                                    <li>
+                                                                        <strong>{usecase.name}:</strong>{' '}
+                                                                        {usecase?.description}
+                                                                    </li>
+                                                                );
+                                                            })}
+                                                        </ul>
+                                                    </div>
+                                                )}
+                                                {feature?.more_content?.features?.length > 0 && (
+                                                    <div
+                                                        className={` ${
+                                                            openedFeatures.includes(index) ? 'block' : 'hidden'
+                                                        }`}
+                                                    >
+                                                        <h4 className='font-semibold text-lg'>Key Features:</h4>
+                                                        <ul className='flex flex-col gap-1 list-disc'>
+                                                            {feature?.more_content?.features?.map((feature, index) => {
+                                                                return <li>{feature}</li>;
+                                                            })}
+                                                        </ul>
+                                                    </div>
+                                                )}
+                                                <div
+                                                    onClick={() => {
+                                                        setOpenedFeatures((prevOpenedFeatures) => {
+                                                            if (prevOpenedFeatures.includes(index)) {
+                                                                return prevOpenedFeatures.filter((i) => i !== index);
+                                                            } else {
+                                                                return [...prevOpenedFeatures, index];
+                                                            }
+                                                        });
+                                                    }}
+                                                >
+                                                    <MoreButton
+                                                        open={openedFeatures.includes(index)}
+                                                        content={'Read More'}
+                                                    />
+                                                </div>{' '}
+                                            </>
+                                        )}
                                     </div>
                                     <Image
                                         src={feature?.img}
