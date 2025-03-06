a057b0415bc483f2017f16e0e9f77289099ddea9
piyusssshh <piyush@whozzat.com>
2025-03-06 14:56:46 +0530

    WEB91-71 | shorturl pricing

--- src/components/PricingComp/PricingNavComp/PricingNavComp.js
+++ src/components/PricingComp/PricingNavComp/PricingNavComp.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function PricingNav({ products, page }) {
-    console.log('🚀 ~ PricingNav ~ products:', products);
     return (
         <>
             <div className='flex w-full justify-start md:hidden '>
