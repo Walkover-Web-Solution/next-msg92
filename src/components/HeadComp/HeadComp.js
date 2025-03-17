2951842bda2bf41208aa22b7857db5d53e8ed537
piyusssshh <piyush@whozzat.com>
2025-03-17 15:59:58 +0530

    WEB91-72 | hreflang

--- src/components/HeadComp/HeadComp.js
+++ src/components/HeadComp/HeadComp.js
@@ -3,7 +3,6 @@ import Script from 'next/script';
 import HreflangTagComp from './HreflangTagComp';
 
 export default function HeadComp({ data, pageInfo }) {
-    console.log('⚡️ ~ HeadComp.js:6 ~ HeadComp ~ data:', data);
     return (
         <>
             <Script
--- src/components/HeadComp/HreflangTagComp.js
+++ src/components/HeadComp/HreflangTagComp.js
@@ -5,7 +5,7 @@ export default function HreflangTagComp({ pageInfo }) {
     return (
         <Head>
             <link rel='canonical' href={`https://msg91.com/${pageInfo?.pathURL}`} />
-            <link rel='alternate' hreflang='en' href={`https://msg91.com/${pageInfo?.baseURL}`} />
+            <link rel='alternate' hrefLang='en' href={`https://msg91.com/${pageInfo?.baseURL}`} />
             {specialPages?.countries.map((country, index) => {
                 if (
                     !specialPages?.global.includes(pageInfo?.page) &&
