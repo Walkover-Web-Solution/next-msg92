2cb4e8bc41104e8dba8d6f840406cdc5fb86081c
piyusssshh <piyush@whozzat.com>
2025-03-20 10:55:42 +0530

    WEB91-70 | get simplified pricing function

--- src/components/PricingComp/PricingSegmento/PricingSegmento.js
+++ src/components/PricingComp/PricingSegmento/PricingSegmento.js
@@ -1,4 +1,3 @@
-import getSubscriptions from '@/utils/getSubscription';
 import { useState, useEffect, useCallback } from 'react';
 import { MdCheck, MdClose } from 'react-icons/md';
 import ConnectWithTeam from '../ConnectWithTeam/ConnectWithTeam';
@@ -9,6 +8,7 @@ import getURL from '@/utils/getURL';
 import CalculatePricingSegmento from './CalculatePricingSegmento/CalculatePricingSegmento';
 import features from '@/data/segmentoPricingFeatures.json';
 import FeaturesModalComp from './FeaturesModalComp/FeaturesModalComp';
+import getSubscriptions from '@/utils/getSimplifiedPlans';
 
 export default function PricingSegmento({ data, country }) {
     const { currency, symbol } = GetCurrencySymbol(country);
@@ -111,8 +111,16 @@ export default function PricingSegmento({ data, country }) {
     }
     return (
         <>
-            <div className='flex flex-col gap-3 w-full'>
-                <h1 className='text-3xl font-semibold capitalize '>segmento Pricing</h1>
+            <div className='flex flex-col gap-6 w-full'>
+                <div className='flex items-center justify-between'>
+                    <h1 className='text-3xl font-semibold capitalize '>segmento Pricing</h1>
+                    <button
+                        className=' btn btn-accent btn-outline btn-sm'
+                        onClick={() => setIsCalculationModalOpen(true)}
+                    >
+                        Calculate
+                    </button>
+                </div>
                 <div className='flex flex-col w-full gap-8'>
                     {hasyYarly && (
                         <div role='tablist' className='tabs tabs-boxed p-0 w-fit'>
@@ -369,14 +377,7 @@ export default function PricingSegmento({ data, country }) {
                                                                     ))}
                                                             </div>
                                                         </div>
-                                                        {/* {plan?.name !== 'Free' && (
-                                                        <button
-                                                            className=' btn btn-accent btn-outline btn-md '
-                                                            onClick={() => setIsCalculationModalOpen(true)}
-                                                        >
-                                                            Calculate
-                                                        </button>
-                                                    )} */}
+
                                                         {isFeatureModalOpen && openedFeatureModal === plan?.name && (
                                                             <dialog className='modal modal-scrollable absolute' open>
                                                                 <div className='bg-white w-[95%] h-[95%] relative rounded-md border modal-box flex flex-col '>
@@ -406,6 +407,178 @@ export default function PricingSegmento({ data, country }) {
                                             );
                                     })
                             )}
+                        {plans?.length > 0 &&
+                            plans?.map((plan, index) => {
+                                return (
+                                    <>
+                                        <div
+                                            key={plan?.name + index}
+                                            className={`flex flex-col gap-4 col-span-1 p-6  rounded bg-white relative ${
+                                                plan?.name === 'Starter' ? 'border-4 border-black' : 'border'
+                                            }`}
+                                        >
+                                            <div className='flex flex-col gap-2'>
+                                                <div className='flex items-center justify-between'>
+                                                    <h2 className=' text-2xl font-semibold'>{plan?.name}</h2>
+                                                    {plan?.name === 'Starter' && (
+                                                        <span className='bg-black text-white px-2 rounded-badge'>
+                                                            Popular
+                                                        </span>
+                                                    )}
+                                                </div>
+                                                <div className='gap-1 flex flex-col'>
+                                                    <p className=' text-2xl font-semibold text-green-600 capitalize'>
+                                                        {symbol}
+                                                        {plan?.amount} {tabtype}
+                                                    </p>
+                                                    {plan?.amount > 0 ? (
+                                                        <p className='text-sm'>
+                                                            {currency === 'INR'
+                                                                ? '+18% GST'
+                                                                : currency === 'GBP'
+                                                                  ? '+VAT'
+                                                                  : '-'}
+                                                        </p>
+                                                    ) : (
+                                                        '-'
+                                                    )}
+                                                    {/* <div>{handleOfferPrice(amount) || '-'}</div> */}
+                                                </div>
+
+                                                <Link href={getURL('signup', 'segmento')} target='_blank'>
+                                                    <button
+                                                        className={`btn btn-primary  btn-sm ${
+                                                            plan?.name === 'Starter' ? '' : 'btn-outline'
+                                                        }`}
+                                                    >
+                                                        Get Started
+                                                    </button>
+                                                </Link>
+                                            </div>
+                                            <span className='border-b-[1px]'></span>
+
+                                            {/* included */}
+                                            <div className='flex flex-col gap-2'>
+                                                <h3 className='text-lg font-semibold'>Included</h3>
+                                                <div className='flex flex-col gap-1'>
+                                                    {plan?.included?.map((service, index) => (
+                                                        <p key={index}>
+                                                            {`${
+                                                                service?.amount === -1 ? 'Unlimited' : service?.amount
+                                                            } ${service.service_name}${
+                                                                service?.amount === -1 ? '' : '/month'
+                                                            }`}
+                                                        </p>
+                                                    ))}
+                                                </div>
+                                            </div>
+
+                                            {/* features */}
+                                            <div className='flex flex-col gap-2'>
+                                                <h3 className='text-lg font-semibold'>Features</h3>
+                                                <div className='flex flex-col gap-1'>
+                                                    {features?.primary?.length > 0 &&
+                                                        features.primary.map((feature, index) => {
+                                                            return (
+                                                                <p
+                                                                    className='flex text-start gap-1 w-fit hover:underline tooltip tooltip-black cursor-pointer'
+                                                                    key={index}
+                                                                    data-tip={feature?.description}
+                                                                >
+                                                                    {feature?.notIncluded &&
+                                                                    feature?.notIncluded.includes(plan?.name) ? (
+                                                                        <MdClose
+                                                                            className='mt-1'
+                                                                            fontSize={20}
+                                                                            color='#DC3645'
+                                                                        />
+                                                                    ) : (
+                                                                        <MdCheck
+                                                                            className='mt-1'
+                                                                            fontSize={20}
+                                                                            color='#16A34A'
+                                                                        />
+                                                                    )}
+
+                                                                    {feature?.name}
+                                                                </p>
+                                                            );
+                                                        })}
+                                                </div>
+                                                <p
+                                                    onClick={() => {
+                                                        setIsFeatureModalOpen(true);
+                                                        setOpenedFeatureModal(plan?.name);
+                                                    }}
+                                                    className='text-link active-link text-sm'
+                                                >
+                                                    See More Features
+                                                </p>
+                                            </div>
+
+                                            {/* Extras */}
+                                            <div className='flex flex-col gap-2'>
+                                                <h3 className='text-lg font-semibold'>Extra @</h3>
+                                                <div className='flex flex-col gap-1'>
+                                                    {plan?.extras?.length > 0 &&
+                                                        plan?.extras.map((service, index) => (
+                                                            <div key={index} className='flex  gap-1'>
+                                                                {service?.rate > 0 ? (
+                                                                    <>
+                                                                        {' '}
+                                                                        <MdCheck
+                                                                            className='mt-1'
+                                                                            fontSize={20}
+                                                                            color='#16A34A'
+                                                                        />
+                                                                        <p>
+                                                                            {`${symbol}${service?.rate}/${
+                                                                                service?.chunk > 1 ? service?.chunk : ''
+                                                                            } ${service?.service_name}/month`}
+                                                                        </p>
+                                                                    </>
+                                                                ) : (
+                                                                    <>
+                                                                        {' '}
+                                                                        <MdClose
+                                                                            className='mt-1'
+                                                                            fontSize={20}
+                                                                            color='#DC3645'
+                                                                        />
+                                                                        <p> No Extra {service?.service_name}</p>
+                                                                    </>
+                                                                )}
+                                                            </div>
+                                                        ))}
+                                                </div>
+                                            </div>
+
+                                            {isFeatureModalOpen && openedFeatureModal === plan?.name && (
+                                                <dialog className='modal modal-scrollable absolute' open>
+                                                    <div className='bg-white w-[95%] h-[95%] relative rounded-md border modal-box flex flex-col '>
+                                                        <div className='flex items-center justify-between h-fit'>
+                                                            <h3 className='text-2xl font-bold'>More Features</h3>
+                                                            <button
+                                                                onClick={() => {
+                                                                    setIsFeatureModalOpen(false);
+                                                                    setOpenedFeatureModal('');
+                                                                }}
+                                                                className='btn btn-sm btn-circle btn-ghost'
+                                                            >
+                                                                ✕
+                                                            </button>
+                                                        </div>
+                                                        <FeaturesModalComp
+                                                            features={features?.modal}
+                                                            plan_name={plan?.name}
+                                                        />
+                                                    </div>
+                                                </dialog>
+                                            )}
+                                        </div>
+                                    </>
+                                );
+                            })}
 
                         {isLoading &&
                             [...Array(3)].map((_, index) => (
@@ -443,11 +616,24 @@ export default function PricingSegmento({ data, country }) {
                     <FaqsComp data={data?.faqComp} notCont={true} />
                 </div>
             </div>
-            {/* {plans && isCalculationModalOpen && (
-                <dialog id='calculate_segmento_pricing' className='modal' open>
-                    <CalculatePricingSegmento />
+            {plans && isCalculationModalOpen && (
+                <dialog id='calculate_segmento_pricing' className='modal z-[1000!important]' open>
+                    <div className='modal-box rounded-md flex flex-col gap-4 justify-between'>
+                        <div className='flex items-center justify-between'>
+                            <h3 className='font-bold text-lg'>Calculate Email Pricing</h3>
+                            <span
+                                className='btn btn-sm btn-circle btn-ghost '
+                                onClick={() => {
+                                    setIsCalculationModalOpen(false);
+                                }}
+                            >
+                                ✕
+                            </span>
+                        </div>
+                        <CalculatePricingSegmento plans={plans} currency={currency} symbol={symbol} tabtype={tabtype} />
+                    </div>
                 </dialog>
-            )} */}
+            )}
         </>
     );
 }
