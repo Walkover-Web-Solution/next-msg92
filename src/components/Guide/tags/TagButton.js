f4700048d2c0bce9a1173c38d3355e1c16ec4be9
piyusssshh <piyush@whozzat.com>
2025-04-30 12:05:30 +0530

    WEB91-91 | updated blog page css

--- src/components/Guide/tags/TagButton.js
+++ src/components/Guide/tags/TagButton.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 export default function TagButton({ tag }) {
     return (
         <>
-            <div className='bg-gray-300 py-1 px-3 rounded-full'>{tag}</div>
+            <div className='bg-gray-200 py-1 px-4 rounded-full'>{tag}</div>
             {/* <Link
                 className='bg-gray-300 py-1 px-3 rounded-full'
                 legacyBehavior
