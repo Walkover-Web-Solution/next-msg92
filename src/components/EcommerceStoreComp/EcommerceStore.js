056ebae98c369388fbc522e880fa5b166fac8608
rashi khandelwal <rashikhandelwal269@gmail.com>
2025-05-03 17:17:18 +0530

    description added

--- src/components/EcommerceStoreComp/EcommerceStore.js
+++ src/components/EcommerceStoreComp/EcommerceStore.js
@@ -1,7 +1,12 @@
+import ecommerceStore from '@/data/global/ecommerce-store.json';
 export default function EcommerceStoreComp() {
     return (
         <>
-            <div className='bg-neutral py-20'>ecommerce store</div>
+            <div className='bg-neutral py-20'>
+                <div className='container'>
+                    <p className='text-gray-500'>{ecommerceStore.HeadComp.description}</p>
+                </div>
+            </div>
         </>
     );
 }
