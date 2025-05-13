c595470f52b6189fe8163f9416b013b318ef6a98
piyusssshh <piyush@whozzat.com>
2025-05-13 11:52:44 +0530

    WEB91-98 | gray color issue

--- src/components/DemoChatBotComp/DemoChatBotComp.js
+++ src/components/DemoChatBotComp/DemoChatBotComp.js
@@ -24,12 +24,12 @@ export default function DemoChatBotComp() {
                             <h2 className='text-xl font-bold'>WhatsApp Bots</h2>
                         </div>
                         <Link href='https://wa.me/917316914316?text=%F0%9F%9B%92%20Track%20My%20Order' target='_blank'>
-                            <button className='btn bg-gray hover:bg-gray-500 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
+                            <button className='btn bg-gray-500 hover:bg-gray-600 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
                                 Track Order
                             </button>
                         </Link>
                         <Link href='https://wa.me/917316914316?text=%F0%9F%93%A6%20Order%20Issues' target='_blank'>
-                            <button className='btn bg-gray hover:bg-gray-500 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
+                            <button className='btn bg-gray-500 hover:bg-gray-600 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
                                 Order Issues
                             </button>
                         </Link>
@@ -37,17 +37,17 @@ export default function DemoChatBotComp() {
                             href='https://wa.me/917316914316?text=%F0%9F%92%B0%20Refund%20%26%20Cancellation'
                             target='_blank'
                         >
-                            <button className='btn bg-gray hover:bg-gray-500 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
+                            <button className='btn bg-gray-500 hover:bg-gray-600 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
                                 Refund and Cancellation
                             </button>
                         </Link>
                         <Link href='https://wa.me/917316914316?text=%F0%9F%8E%81%20Ongoing%20Offers' target='_blank'>
-                            <button className='btn bg-gray hover:bg-gray-500 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
+                            <button className='btn bg-gray-500 hover:bg-gray-600 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
                                 Ongoing Offers
                             </button>
                         </Link>
                         <Link href='https://wa.me/917316914316?text=%F0%9F%99%8B%20Talk%20to%20Support' target='_blank'>
-                            <button className='btn bg-gray hover:bg-gray-500 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
+                            <button className='btn bg-gray-500 hover:bg-gray-600 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
                                 Talk to Support
                             </button>
                         </Link>
@@ -55,12 +55,12 @@ export default function DemoChatBotComp() {
                             href='https://wa.me/917316914316?text=%F0%9F%9B%92%E2%8F%B3Abandoned%20Cart'
                             target='_blank'
                         >
-                            <button className='btn bg-gray hover:bg-gray-500 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
+                            <button className='btn bg-gray-500 hover:bg-gray-600 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
                                 Abandoned Cart
                             </button>
                         </Link>
                         <Link href='https://wa.me/917316914316?text=Latest%20Collection' target='_blank'>
-                            <button className='btn bg-gray hover:bg-gray-500 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
+                            <button className='btn bg-gray-500 hover:bg-gray-600 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
                                 Latest Collection
                             </button>
                         </Link>
@@ -100,12 +100,12 @@ export default function DemoChatBotComp() {
                                 <h2 className='text-xl font-bold'>Instagram Bots</h2>
                             </div>
                             <Link href='https://ig.me/m/MSG91_Instabot' target='_blank'>
-                                <button className='btn bg-gray hover:bg-gray-500 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
+                                <button className='btn bg-gray-500 hover:bg-gray-600 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
                                     Fashion & Style Industry
                                 </button>
                             </Link>
                             <Link href='https://ig.me/m/msg91_hello' target='_blank'>
-                                <button className='btn bg-gray hover:bg-gray-500 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
+                                <button className='btn bg-gray-500 hover:bg-gray-600 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
                                     Real Estate Industry
                                 </button>
                             </Link>
@@ -139,7 +139,7 @@ export default function DemoChatBotComp() {
                                 <h2 className='text-xl font-bold'>Website Bots</h2>
                             </div>
                             <div onClick={() => window.chatWidget.open()}>
-                                <button className='btn bg-gray hover:bg-gray-500 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
+                                <button className='btn bg-gray-500 hover:bg-gray-600 border-none font-bold h-[44px] min-h-[36px] text-[16px] text-white w-[300px]'>
                                     Marketing Strategy
                                 </button>
                                 {/* <div>
