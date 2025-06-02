41154f4105c061ccdd275eb5c8fd9ee489dfac7c
piyusssshh <piyush@whozzat.com>
2025-06-02 14:44:00 +0530

    WEB91-112 | lang tag issue

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,26 +1,47 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Document from 'next/document';
-import availableCountries from '@/data/availableCountries.json';
-
-function getLanguageFromPath(path) {
-    const countryFromPath = path?.split('/')[1]?.toUpperCase() || 'GLOBAL';
-    const countryInfo = availableCountries.find((country) => country.shortname.toUpperCase() === countryFromPath);
-    return countryInfo?.htmlLang || 'en';
-}
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx);
-        const path = ctx.req?.url || '';
-        const lang = getLanguageFromPath(path);
+
+        let pathname = '';
+        if (ctx.req) {
+            const url = ctx.req.url || '';
+            pathname = url.split('?')[0];
+            if (pathname.startsWith('/')) {
+                pathname = pathname.slice(1);
+            }
+        }
+        let htmlLang;
+        if (pathname && !pathname.startsWith('.well-known')) {
+            const pathNameArray = pathname?.split('/') || [];
+            try {
+                const availableCountries = require('../data/availableCountries.json');
+                const country = availableCountries.find(
+                    (c) => c.shortname && c.shortname.toLowerCase() === pathNameArray[0]?.toLowerCase()
+                );
+                if (country && country?.htmlLang) {
+                    htmlLang = country.htmlLang;
+                } else {
+                    htmlLang = 'en';
+                }
+            } catch (e) {
+                // fallback logic if needed
+                htmlLang = 'en';
+            }
+        } else {
+            // htmlLang = 'en';
+        }
+        console.log('⚡️ ~ :36 ~ MyDocument ~ getInitialProps ~ htmlLang:', htmlLang);
         return {
             ...initialProps,
-            lang,
+            htmlLang,
         };
     }
     render() {
         return (
-            <Html lang={this.props.lang} data-theme='light'>
+            <Html lang={this.props.htmlLang} data-theme='light' data-base-url={this.props.baseUrl}>
                 <Head />
                 <body>
                     <Main />
