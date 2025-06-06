966c61434736c77e99fb1b051fd58b80846be071
piyusssshh <piyush@whozzat.com>
2025-06-06 15:09:37 +0530

    WEB91-120 | mammybeans contnet update

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -54,7 +54,7 @@ import specialPages from '@/data/specialPages.json';
 import { useRouter } from 'next/router';
 import PreFooterMFTL from '@/components/migrateFromTextlocal/preFooterMFTL/preFooterMFTL';
 import CTAMFTL from '@/components/migrateFromTextlocal/ctaMFTL/ctaMFTL';
-import CaseStudyPageComp from '@/components/CaseStudyComp/caseStudyPageComp/CaseStudyPageComp';
+import CaseStudyPageComp from '@/components/CaseStudyComp/CaseStudyPageComp/CaseStudyPageComp';
 
 const Components = {
     BannerComp,
