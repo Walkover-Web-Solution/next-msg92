88c07818c39616a413293fb7f5156b8c35125aec
piyusssshh <piyush@whozzat.com>
2025-06-20 15:52:54 +0530

    UPDATE | menu responsive design

--- src/components/Navbar/NavbarUtils/index.js
+++ src/components/Navbar/NavbarUtils/index.js
@@ -1,8 +1,7 @@
 'use client';
 import Image from 'next/image';
-import styles from '../Navbar.module.scss';
-import { MdMenu } from 'react-icons/md';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { MdMenu, MdMenuOpen } from 'react-icons/md';
 export function NavbarUtils() {
     function handleMenuHover() {
         const navMain = document.getElementById('nav_main');
@@ -10,14 +9,31 @@ export function NavbarUtils() {
         if (navMain && !navMain.classList.contains('show')) {
             navMain.classList.add('show');
             navOverlay.classList.add('show');
+            document.body.classList.add('overflow-hidden');
         }
+    }
+    function handleProductHover() {
+        handleMenuHover();
+        const navProducts = document.getElementById('nav_products');
+        const navIntegrations = document.getElementById('nav_integrations');
+        navProducts.classList.remove('hidden');
+        navProducts.classList.add('flex');
+        navIntegrations.classList.add('hidden');
+    }
+    function handleIntegrationsHover() {
+        handleMenuHover();
+        const navProducts = document.getElementById('nav_products');
+        const navIntegrations = document.getElementById('nav_integrations');
+        navProducts.classList.add('hidden');
+        navIntegrations.classList.remove('hidden');
+        navIntegrations.classList.add('flex');
     }
     return (
         <>
             <li
                 className='text-md font-medium text-link flex items-center'
                 onMouseEnter={() => {
-                    handleMenuHover();
+                    handleProductHover();
                 }}
             >
                 <Image
@@ -29,35 +45,79 @@ export function NavbarUtils() {
                 />
                 Hello
             </li>
-            <li      onMouseEnter={() => {
-                    handleMenuHover();
-                }} className='text-md font-medium text-link'>Integrations</li>
+            <li
+                onMouseEnter={() => {
+                    handleIntegrationsHover();
+                }}
+                className='text-md font-medium text-link'
+            >
+                Integrations
+            </li>
         </>
     );
 }
 
 export function MiniMenuButton() {
+    const [navOpen, setNavOpen] = useState(false);
+    function handleProductOpen() {
+        setNavOpen(true);
+        const navProducts = document.getElementById('nav_products');
+        const navMain = document.getElementById('nav_main');
+        const navOverlay = document.getElementById('nav_overlay');
+        navMain.classList.add('show');
+        document.body.classList.add('overflow-hidden');
+        navProducts.classList.remove('hidden');
+        navProducts.classList.add('flex');
+    }
+    function handleProductClose() {
+        setNavOpen(false);
+        const navProducts = document.getElementById('nav_products');
+        const navMain = document.getElementById('nav_main');
+        const navOverlay = document.getElementById('nav_overlay');
+        navMain.classList.remove('show');
+        document.body.classList.remove('overflow-hidden');
+        navProducts.classList.add('hidden');
+        navProducts.classList.remove('flex');
+    }
     return (
         <>
-            <MdMenu className='text-3xl' />
+            {navOpen ? (
+                <MdMenuOpen
+                    onClick={() => {
+                        handleProductClose();
+                    }}
+                    className='text-3xl'
+                />
+            ) : (
+                <MdMenu
+                    onClick={() => {
+                        handleProductOpen();
+                    }}
+                    className='text-3xl'
+                />
+            )}
         </>
     );
 }
 export function NavbarBlank() {
     function handleBlankHover() {
-        console.log('⚡️ ~ :46 ~ handleBlankHover ~ handleBlankHover');
-
         const navMain = document.getElementById('nav_main');
         const navOverlay = document.getElementById('nav_overlay');
+        const navProducts = document.getElementById('nav_products');
+        const navIntegrations = document.getElementById('nav_integrations');
+
         if ((navMain && navMain.classList.contains('show')) || (navOverlay && navMain.classList.contains('show'))) {
             navMain.classList.remove('show');
             navOverlay.classList.remove('show');
+            document.body.classList.remove('overflow-hidden');
+            navProducts.classList.add('hidden');
+            navIntegrations.classList.add('hidden');
         }
     }
     return (
         <div
             id='nav_bg_blank'
-            className={'w-full '}
+            className={'w-full hidden md:block '}
             onMouseEnter={() => {
                 handleBlankHover();
             }}
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from './Navbar.module.scss';
 import { MiniMenuButton, NavbarBlank, NavbarUtils } from './NavbarUtils';
+import { LinkButton, LinkText } from '../1UIComponents/Buttons';
 
 export default function Navbar({ data, pageInfo }) {
     return (
@@ -26,7 +27,9 @@ export default function Navbar({ data, pageInfo }) {
             {/* //LG Navigation End */}
 
             {/* Small Navigation */}
-            <div className='container py-2 flex md:hidden items-center justify-between'>
+            <div
+                className={`${styles.navigation} container py-2 flex md:hidden items-center justify-between relative bg-white`}
+            >
                 <Image
                     width={360}
                     height={120}
@@ -38,22 +41,22 @@ export default function Navbar({ data, pageInfo }) {
             </div>
             {/* Small Navigation */}
             {/* Menu Content */}
-            <div id='nav_overlay' className={`${styles.overlay} `}></div>
+            <div id='nav_overlay' className={`${styles.overlay}`}></div>
 
             <div id='nav_main' className={`${styles.mainnav} `}>
-                <div id='nav_bg' className={styles.background}></div>
+                <div id='nav_bg' className={`${styles.background} w-full lg:min-w-[920px] md:min-w-[600px] md:max-w-[86%]`}></div>
                 <NavbarBlank />
                 <div id='nav_cont' className={styles.cont}>
-                    <div className='container flex h-full'>
-                        <div className='w-1/2 min-w-[720px] h-fit mt-30 flex pe-10 flex-wrap items-start justify-start'>
+                    <div className='container flex h-full overflow-y-scroll'>
+                        <div id='nav_products' className='hidden w-full lg:min-w-[720px] md:min-w-[340px] mt-32 flex-wrap h-fit md:max-w-[86%]'>
                             {data?.products_list?.map((list, idx) => (
-                                <div key={idx} className='mb-10 pe-10 w-1/2 max-h-fit'>
+                                <div key={idx} className='mb-10 pe-10 lg:w-1/2 md:w-full sm:w-1/2 w-full max-h-fit'>
                                     <span className='font-semibold text-sm text-gray-600'>{list?.name}</span>
                                     <ul className='cont gap-2'>
                                         {list.products?.map((product, pidx) => (
                                             <li
                                                 key={pidx}
-                                                className='flex items-center gap-2 w-full hover:bg-secondary rounded p-2 '
+                                                className='flex items-center gap-2 w-full hover:bg-secondary rounded p-2 LinkButtonCard'
                                             >
                                                 <Image
                                                     className='w-fit h-10'
@@ -63,9 +66,11 @@ export default function Navbar({ data, pageInfo }) {
                                                     alt={`${product?.name} Logo`}
                                                 />
                                                 <div className='cont'>
-                                                    <span className='text-lg font-bold'>{product?.name}</span>
+                                                    <LinkText customClasses={'text-base font-semibold'}>
+                                                        {product?.name}
+                                                    </LinkText>
                                                     {product?.description && (
-                                                        <span className='text-sm text-gray-600'>
+                                                        <span className='text-sm text-gray-500'>
                                                             {product?.description}
                                                         </span>
                                                     )}
@@ -75,6 +80,41 @@ export default function Navbar({ data, pageInfo }) {
                                     </ul>
                                 </div>
                             ))}
+                        </div>
+                        <div id='nav_integrations' className='hidden w-full lg:min-w-[720px] md:min-w-[340px] mt-32 flex-wrap h-fit md:max-w-[86%]'>
+                            {data?.integrations_list && (
+                                <ul className='cont gap-4 lg:w-1/2 md:w-full sm:w-1/2 w-full max-h-fit'>
+                                    {data?.integrations_list?.content?.map((product, pidx) => (
+                                        <li
+                                            key={pidx}
+                                            className='flex items-center gap-4 w-full hover:bg-secondary rounded px-4 py-2 LinkButtonCard'
+                                        >
+                                            <Image
+                                                className='w-10 h-fit max-h-10'
+                                                src={product?.icon}
+                                                width={72}
+                                                height={72}
+                                                alt={`${product?.name} Logo`}
+                                            />
+                                            <div className='cont'>
+                                                <LinkText customClasses={'text-base font-semibold'}>
+                                                    {product?.name}
+                                                </LinkText>
+                                                {product?.description && (
+                                                    <span className='text-sm text-gray-600'>
+                                                        {product?.description}
+                                                    </span>
+                                                )}
+                                            </div>
+                                        </li>
+                                    ))}
+                                    <LinkButton
+                                        href={'/'}
+                                        content={data?.integrations_list?.explore_btn}
+                                        customClasses={'text-base font-semibold text-link'}
+                                    ></LinkButton>
+                                </ul>
+                            )}
                         </div>
                         <NavbarBlank />
                     </div>
