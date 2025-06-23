f4c120939d90c4407f4bc9b15470d3b24b7df81e
piyusssshh <piyush@whozzat.com>
2025-06-23 15:35:11 +0530

    WEB91-128 | removed mm lite from pricing

--- src/components/PricingComp/PricingWhatsApp/PricingWhatsApp.js
+++ src/components/PricingComp/PricingWhatsApp/PricingWhatsApp.js
@@ -13,6 +13,7 @@ export default function PricingWhatsApp({ country, data }) {
     const currentCountry = GetCountryDetails({ shortname: country, type: 'shortname' });
     const [loading, setLoading] = useState(true);
     const [plans, setPlans] = useState();
+    console.log('⚡️ ~ :16 ~ PricingWhatsApp ~ plans:', plans);
 
     useEffect(() => {
         const getWhatsAppPricing = async () => {
@@ -57,25 +58,23 @@ export default function PricingWhatsApp({ country, data }) {
                         />
                     </div>
                     <div className='flex flex-col gap-8 w-full'>
-                        {(data?.heading || data?.tax || data?.adds) && (
-                            <div className='flex flex-col gap-2'>
-                                <div className='flex flex-col md:flex-row w-full justify-between ms:items-center gap-1'>
-                                    {data?.heading && <h2 className='text-2xl font-bold '>{data?.heading} </h2>}
-                                    {plans && plans?.length > 0 && (
-                                        <button
-                                            onClick={() =>
-                                                document.getElementById('calculate_whatsapp_pricing').showModal()
-                                            }
-                                            className='btn btn-accent btn-outline w-fit btn-sm'
-                                        >
-                                            Calculate
-                                        </button>
-                                    )}
-                                </div>
-                                <p>{data?.tax}</p>
-                                {data?.adds && <p>{data?.adds}</p>}
+                        <div className='flex flex-col gap-2'>
+                            <div className='flex flex-col md:flex-row w-full justify-between ms:items-center gap-1'>
+                                {data?.heading && <h2 className='text-2xl font-bold '>{data?.heading} </h2>}
+                                {plans && plans?.length > 0 && (
+                                    <button
+                                        onClick={() =>
+                                            document.getElementById('calculate_whatsapp_pricing').showModal()
+                                        }
+                                        className='btn btn-accent btn-outline w-fit btn-sm ms-auto'
+                                    >
+                                        Calculate
+                                    </button>
+                                )}
                             </div>
-                        )}
+                            <p>{data?.tax}</p>
+                            {data?.adds && <p>{data?.adds}</p>}
+                        </div>
 
                         {!loading ? (
                             <div className={styles?.table_cont}>
@@ -86,9 +85,9 @@ export default function PricingWhatsApp({ country, data }) {
                                             <span className='border-r'>Prefix</span>
                                             <span className='border-r'>Marketing</span>
                                             <span className='border-r'>Utility</span>
-                                            <span className='border-r'>Authentication</span>
-                                            <span className='border-r'>Service</span>
-                                            <span className=''>MM Lite</span>
+                                            <span className=''>Authentication</span>
+                                            {/* <span className='border-r'>Service</span> */}
+                                            {/* <span className=''>MM Lite</span> */}
                                         </div>
                                         {plans?.map((item, index) => {
                                             if (currentCountry.name === item.country_name) {
@@ -117,7 +116,7 @@ export default function PricingWhatsApp({ country, data }) {
                             <div className='cont cont_gap p-4 bg-white w-full'>
                                 {Array.from({ length: 20 }).map((_, index) => (
                                     <div key={index} className='flex gap-4'>
-                                        {Array.from({ length: 7 }).map((_, i) => (
+                                        {Array.from({ length: 5 }).map((_, i) => (
                                             <div
                                                 key={i}
                                                 className='skeleton bg-gray-500 text-gray-400 w-full h-6 rounded-sm'
@@ -153,9 +152,9 @@ function RowComp({ item, index, symbol }) {
             <span className='border-r'>{item?.prefix === 0 ? 'N/A' : item?.prefix}</span>
             <span className='border-r'>{formatRate(item?.marketing_rate)}</span>
             <span className='border-r'>{formatRate(item?.utility_rate)}</span>
-            <span className='border-r'>{formatRate(item?.authentication_rate)}</span>
-            <span className='border-r'>{formatRate(item?.user_initiated_rate)}</span>
-            <span>{formatRate(item?.mm_lite_rate)}</span>
+            <span className=''>{formatRate(item?.authentication_rate)}</span>
+            {/* <span className='border-r'>{formatRate(item?.user_initiated_rate)}</span> */}
+            {/* <span>{formatRate(item?.mm_lite_rate)}</span> */}
         </div>
     );
 }
