f43c4901b99981f584c8326919fca37b2bc44b55
piyusssshh <piyush@whozzat.com>
2025-06-26 12:17:03 +0530

    WEB91-130 | 404 page

--- src/components/NotFoundComp/NotFoundComp.js
+++ src/components/NotFoundComp/NotFoundComp.js
@@ -1,19 +1,32 @@
-import Link from 'next/link';
+import Image from 'next/image';
+import { MdArrowBack } from 'react-icons/md';
 
 export default function NotFoundComp({ pageInfo }) {
     return (
-        <div className='flex flex-col items-center container py-40 cont_gap'>
-            <span className='text-8xl'>404</span>
-            <h1 className='text-4xl font-semibold'>
-                The page you're trying to reach isn't available or doesn't exist.
-            </h1>
-            <div className='flex items-center gap-4'>
-                <a className='btn btn-secondary' href={pageInfo?.country === 'global' ? '/' : pageInfo?.country}>
-                    Go To Home
-                </a>
-                <a className='btn btn-secondary' href={'/contact-us'}>
-                    Contact Support
-                </a>
+        <div className='flex flex-col items-center container py-12 cont_gap'>
+            <Image
+                src={'/assets/brand/msg91.svg'}
+                width={120}
+                height={80}
+                className='w-fit h-12 mb-8'
+                alt='MSG91 LOGO'
+            />
+            <div className='flex flex-col items-center py-40 cont_gap'>
+                <span className='text-8xl font-bold'>404</span>
+                <h1 className='text-4xl font-semibold'>
+                    The page you're trying to reach isn't available or doesn't exist.
+                </h1>
+                <div className='flex items-center gap-4'>
+                    <button className='btn btn-primary' onClick={() => window.history.back()} type='button'>
+                        <MdArrowBack /> Go Back
+                    </button>
+                    <a className='btn btn-secondary' href={'/'}>
+                        Go to Homepage
+                    </a>
+                    <a className='btn  btn-ghost' href={'/contact-us'}>
+                        Contact Support
+                    </a>
+                </div>
             </div>
         </div>
     );
