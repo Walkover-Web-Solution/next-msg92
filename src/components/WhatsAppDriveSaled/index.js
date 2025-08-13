8f5f04af3488987ba2286b3a282db1d8b33c9ae0
piyusssshh <piyush@whozzat.com>
2025-08-13 16:07:52 +0530

    WEB91-148 | whatsapp graphics

--- src/components/WhatsAppDriveSaled/index.js
+++ src/components/WhatsAppDriveSaled/index.js
@@ -1,24 +1,29 @@
 import { MdArrowRight, MdArrowRightAlt, MdCheck } from 'react-icons/md';
+import LottiePlayer from '../LottiePlayer/LottiePlayer';
 
 export default function WhatsAppDriveSaled({ data }) {
-    console.log('⚡️ ~ :2 ~ WhatsAppDriveSaled ~ data:', data);
     return (
-        <div className='container cont cont_p cont_gap'>
-            <h2 className='text-3xl font-bold'>{data?.heading}</h2>
-            <div>
-                <p className='font-bold text-xl'>{data?.subheading}</p>
-                <p>{data?.description}</p>
-            </div>
-            <div className='cont cont_gap'>
-                {data?.content.map((feature) => (
-                    <div className='flex gap-1'>
-                        <MdArrowRightAlt className='text-2xl  text-green-600' />
-                        <div className=''>
-                            <h3 className='font-bold'>{feature?.title}</h3>
-                            <p>{feature?.description}</p>
+        <div className='container flex flex-col md:flex-row cont_p cont_gap items-center justify-between'>
+            <div className='cont gap-2'>
+                <h2 className='text-3xl font-bold'>{data?.heading}</h2>
+                <div>
+                    <p className='font-bold text-xl'>{data?.subheading}</p>
+                    <p>{data?.description}</p>
+                </div>
+                <div className='cont cont_gap'>
+                    {data?.content.map((feature) => (
+                        <div className='flex gap-1'>
+                            <MdArrowRightAlt className='text-2xl  text-green-600' />
+                            <div className=''>
+                                <h3 className='font-bold'>{feature?.title}</h3>
+                                <p>{feature?.description}</p>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
+            </div>
+            <div className='w-full xl:max-w-[560px] lg:max-w-[320px] max-w-[280px] md:mx-6 mx-2 rounded'>
+                <LottiePlayer lottie={data?.img} />
             </div>
         </div>
     );
