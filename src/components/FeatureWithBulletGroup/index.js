9c50b0cae7db94ee33ef38e327354dd4b121b5d5
piyusssshh <piyush@whozzat.com>
2025-09-16 13:34:57 +0530

    WEB91-160 | hello feature content update

--- src/components/FeatureWithBulletGroup/index.js
+++ src/components/FeatureWithBulletGroup/index.js
@@ -1,44 +1,45 @@
 import Image from 'next/image';
 import { MdArrowRightAlt } from 'react-icons/md';
 import LottiePlayer from '../LottiePlayer/LottiePlayer';
+import styles from './FeatureWithBulletGroup.module.scss';
 
 export default function FeatureWithBulletGroup({ data }) {
-    console.log('⚡️ ~ FeatureWithBulletGroup ~ data:', data);
-
     if (!data || data.features.length === 0) return null;
-
     return (
-        <div className='container cont cont_gap cont_p'>
+        <div className='container cont gap-10 md:gap-16 cont_p'>
             {data.features.map((group, index) => {
                 return (
                     <div
                         key={index}
-                        className={`flex items-center xl:gap-20 lg:gap-16 md:gap-12 gap-8 ${
+                        className={`flex items-center gap-4 md:gap-12 ${
                             index % 2 === 0 ? 'lg:flex-row flex-col' : 'lg:flex-row-reverse flex-col'
                         }`}
                     >
-                        <div>
-                            <h2 className='text-2xl font-bold text-primary'>{group.name}</h2>
-                            <p className='text-lg text-gray-600'>{group.content}</p>
-                            {group.more_content?.features && (
-                                <ul className='list-disc ps-8 text-lg text-gray-600'>
-                                    {group.more_content.features.map((item, idx) => (
-                                        <li key={idx}>{item}</li>
+                        <div className='w-full flex flex-col gap-2'>
+                            <h2 className='md:text-3xl text-xl font-semibold text-primary'>{group?.name}</h2>
+                            <p className='md:text-lg text-base text-gray-800'>{group?.content}</p>
+                            {group?.more_content?.features?.length > 0 && (
+                                <ul className='text-gray-600 cont gap-1'>
+                                    {group?.more_content?.features.map((item, idx) => (
+                                        <li className='flex gap-2' key={idx}>
+                                            {' '}
+                                            <MdArrowRightAlt className='text-primary text-lg mt-1' /> {item}
+                                        </li>
                                     ))}
                                 </ul>
                             )}
                         </div>
                         {group?.lottie ? (
-                            <div className='w-full xl:max-w-[490px] lg:max-w-[400px] max-w-[300px] md:w-full md:max-w-[600px] '>
+                            <div className={`${styles.image}`}>
                                 <LottiePlayer lottie={group?.lottie} />
                             </div>
                         ) : (
                             <Image
                                 width={720}
                                 height={720}
-                                className='max-w-[clamp(200px, 40%, 500px)] lg:w-1/3 w-full'
+                                className={`${styles?.image}`}
                                 src={group?.img}
-                                alt={group.name}
+                                alt={group?.name}
                             />
                         )}
                     </div>
