676bea55e1d12bea3b75d2e184af049979c81573
Ashish Yadav <ashish@walkover.in>
2025-09-20 18:00:52 +0530

    add calendly and apps link

--- src/components/FeatureWithBulletGroup/index.js
+++ src/components/FeatureWithBulletGroup/index.js
@@ -1,10 +1,22 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import { MdArrowRightAlt } from 'react-icons/md';
 import LottiePlayer from '../LottiePlayer/LottiePlayer';
 import styles from './FeatureWithBulletGroup.module.scss';
+import { LinkText } from '../UIComponent/Buttons/LinkButton';
 
 export default function FeatureWithBulletGroup({ data }) {
+    const [showMobileLinks, setShowMobileLinks] = useState({});
+
     if (!data || data.features.length === 0) return null;
+
+    const toggleMobileLinks = (index) => {
+        setShowMobileLinks((prev) => ({
+            ...prev,
+            [index]: !prev[index],
+        }));
+    };
+
     return (
         <div className='container cont gap-10 md:gap-16 cont_p'>
             {data.features.map((group, index) => {
@@ -30,6 +42,42 @@ export default function FeatureWithBulletGroup({ data }) {
                                         </li>
                                     ))}
                                 </ul>
+                            )}
+                            {group?.schedule_meet && (
+                                <a href='https://calendly.com/hello-shared-inbox/hello-meeting' target='_blank'>
+                                    <LinkText customClasses={'text-link active-link'}>{group?.schedule_meet}</LinkText>
+                                </a>
+                            )}
+                            {group?.get_in_action && (
+                                <a href='/demochatbot' target='_blank'>
+                                    <LinkText customClasses={'text-link active-link'}>{group?.get_in_action}</LinkText>
+                                </a>
+                            )}
+                            {group?.get_mobile_app && (
+                                <div className='flex gap-4 items-center flex-wrap'>
+                                    <button
+                                        className='btn btn-md btn-hello btn-outline'
+                                        onClick={() => toggleMobileLinks(index)}
+                                    >
+                                        <LinkText>Get Mobile App</LinkText>
+                                    </button>
+                                    {showMobileLinks[index] && (
+                                        <>
+                                            <a
+                                                href='https://play.google.com/store/apps/details?id=com.hellornapp&pli=1'
+                                                target='_blank'
+                                            >
+                                                <LinkText customClasses={'text-link active-link'}>Android</LinkText>
+                                            </a>
+                                            <a
+                                                href='https://apps.apple.com/in/app/hello-chatbot-shared-inbox/id1635375551'
+                                                target='_blank'
+                                            >
+                                                <LinkText customClasses={'text-link active-link'}>iOS</LinkText>
+                                            </a>
+                                        </>
+                                    )}
+                                </div>
                             )}
                         </div>
                         {group?.lottie ? (
