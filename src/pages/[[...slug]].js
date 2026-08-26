5b3c0a6528a589aab73472116ab8bd1857c2178c
pallavi-mourya <pallavi@whozzat.com>
2026-08-26 17:39:08 +0530

    Pallavi | separate image section

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -87,7 +87,7 @@ import HelloStartupFooterCta from '@/components/HelloStartupComp/FooterCtaComp/F
 import HelloStartupTerms from '@/components/HelloStartupComp/TermsComp/HelloStartupTerms';
 import CategoriesComp from '@/components/ReportsComp/CategoriesComp/CategoriesComp';
 import SetupStepsComp from '@/components/ReportsComp/SetupStepsComp/SetupStepsComp';
-import HelloAnalyticsComp from '@/components/ReportsComp/HelloAnalyticsComp/HelloAnalyticsComp';
+import ImageWithBgComp from '@/components/ImageWithBgComp/ImageWithBgComp';
 
 /* files */
 import specialPages from '@/data/specialPages.json';
@@ -199,7 +199,7 @@ const Components = {
     HelloStartupTerms,
     CategoriesComp,
     SetupStepsComp,
-    HelloAnalyticsComp,
+    ImageWithBgComp,
 };
 
 export default function Page({ data, commonData, pageInfo }) {
