cd76f0e5aaa58af30c08f473040dd3c6cec80c5d
pallavi-mourya <pallavi@whozzat.com>
2026-09-02 15:33:49 +0530

    Pallavi | fix the changes

--- src/components/PricingComp/constants.js
+++ src/components/PricingComp/constants.js
@@ -17,7 +17,7 @@ export const TAB_TYPES = {
 };
 
 // Searchable fields for dial plan
-export const SEARCHABLE_FIELDS = ['country_name', 'identifier', 'name'];
+export const SEARCHABLE_FIELDS = ['country_name', 'identifier'];
 
 // Empty arrays for memoization
 export const EMPTY_ARRAY = [];
