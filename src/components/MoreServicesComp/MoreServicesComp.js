83009f2182ffc71f1a2ad6890f770f94d9e7d02b
pallavi-mourya <pallavi@whozzat.com>
2026-09-17 13:16:37 +0530

    pallavi | fix the chnages

--- src/components/MoreServicesComp/MoreServicesComp.js
+++ src/components/MoreServicesComp/MoreServicesComp.js
@@ -15,7 +15,18 @@ export default function MoreServicesComp({ data }) {
                                 className='flex flex-col gap-4 bg-neutral rounded-lg p-6 hover:shadow-lg LinkButtonCard'
                                 href={service?.link}
                             >
-                                <div className='flex items-center gap-2'>
+                                <div className='flex items-center justify-center h-[270px] md:h-[300px] w-full'>
+                                    <Image
+                                        width={550}
+                                        height={300}
+                                        className='h-full max-h-[300px] w-auto object-contain mx-auto'
+                                        src={service?.img}
+                                        alt={service?.name}
+                                        loading='lazy'
+                                        sizes='(max-width: 768px) 340px, (max-width: 1024px) 460px, 550px'
+                                    />
+                                </div>
+                                <div className='flex items-center gap-2 mt-2'>
                                     <Image
                                         width={32}
                                         height={32}
@@ -26,19 +37,8 @@ export default function MoreServicesComp({ data }) {
                                     />
                                     <h3 className='text-xl font-semibold'>{service?.name}</h3>
                                 </div>
-                                <div className='flex items-center justify-center h-[200px] w-full'>
-                                    <Image
-                                        width={400}
-                                        height={200}
-                                        className='h-full w-auto object-contain'
-                                        src={service?.img}
-                                        alt={service?.name}
-                                        loading='lazy'
-                                        sizes='(max-width: 768px) 280px, 400px'
-                                    />
-                                </div>
-                                <div className='flex flex-col gap-1 mb-auto'>
-                                    <span className='text-lg font-semibold'>{service?.heading}</span>
+                                <div className='flex flex-col gap-1.5 mb-auto'>
+                                    <h4 className='text-lg font-semibold'>{service?.heading}</h4>
                                     <p className='text-base'>{service?.description}</p>
                                 </div>
                                 <LinkText className='link-btn'>{data?.linkText}</LinkText>
